cf1bc6ddec0ad65e64e21be660803cc489d97635
liadomide <lia_domide@yahoo.com>
2019-08-23 22:19:28 +0300

    HBP-29 Remove indirect dependencies

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 VERSION = "1.0.0"
 
-INSTALL_REQUIREMENTS = ["numpy", "matplotlib", "pyplot",  "tvb-library", "tvb-data"]
+INSTALL_REQUIREMENTS = ["pyplot",  "tvb-library"]
 
 setuptools.setup(name='tvb-nest',
                  version=VERSION,
