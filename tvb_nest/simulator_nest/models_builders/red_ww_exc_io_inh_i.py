602c3d128b609585dad342f97850f922317edb9d
Dionysios Perdikis <dionperd@gmail.com>
2019-10-25 16:08:49 +0200

    Added J_N and J_i as inputs to RedWWExcIOInhIBuilder. Corrected error with output devices overlapping connection names.

--- tvb_nest/simulator_nest/models_builders/red_ww_exc_io_inh_i.py
+++ tvb_nest/simulator_nest/models_builders/red_ww_exc_io_inh_i.py
@@ -8,7 +8,8 @@ from tvb_nest.simulator_nest.models_builders.base import NESTModelBuilder
 
 class RedWWExcIOInhIBuilder(NESTModelBuilder):
 
-    def __init__(self, tvb_simulator, nest_nodes_ids, nest_instance=None, config=CONFIGURED):
+    def __init__(self, tvb_simulator, nest_nodes_ids, nest_instance=None, config=CONFIGURED,
+                 J_N=150, J_i=1000):
         super(RedWWExcIOInhIBuilder, self).__init__(tvb_simulator, nest_nodes_ids, nest_instance, config)
 
         # Common order of neurons' number per population:
@@ -22,15 +23,17 @@ class RedWWExcIOInhIBuilder(NESTModelBuilder):
 
         # Connection weights between the distinct populations:
         # Choosing the values resulting from J_N = 150 pA and J_i = 1000 pA [1]
-        w_ee = 150.0
-        w_ei = -1000.0
-        w_ie = 150.0
-        w_ii = -1000.0
+        self.J_N = J_N
+        self.J_i = J_i
+        w_ee = J_N
+        w_ei = -J_i
+        w_ie = J_N
+        w_ii = -J_i
 
         # Within region-node connections' weights
         self.population_connectivity_synapses_weights = \
-            np.array([[w_ee, w_ei],     # exc_i -> exc_i, inh_i -> exc_i
-                      [w_ie, w_ii]]).T  # exc_i -> inh_i, inh_i -> inh_i
+            np.array([[w_ee, w_ei],   # exc_i -> exc_i, inh_i -> exc_i
+                      [w_ie, w_ii]])  # exc_i -> inh_i, inh_i -> inh_i
         self.population_connectivity_synapses_delays = \
             np.array(self.tvb_dt / 4)
 
@@ -47,12 +50,17 @@ class RedWWExcIOInhIBuilder(NESTModelBuilder):
               "delay": 0.0,
               "receptor_type": 0}]  # additional delay to the one of TVB connectivity
 
-        # Creating spike_detector devices to be able to observe NEST spiking activity:
+        # Creating  devices to be able to observe NEST activity:
+        self.output_devices = []
         connections = OrderedDict({})
         #          label <- target population
-        connections["E"] = "E"
-        connections["I"] = "I"
-        self.output_devices = \
-            [{"model": "spike_detector",
-              "props": config.nest.NEST_OUTPUT_DEVICES_PARAMS_DEF["spike_detector"],
-              "nodes": None, "connections": connections}]
+        connections["E spikes"] = "E"
+        connections["I spikes"] = "I"
+        self.output_devices.append({"model": "spike_detector", "params": {},
+                                    "nodes": None, "connections": connections})
+        connections = OrderedDict({})
+        connections["E V_m"] = "E"
+        connections["I V_m"] = "I"
+        params = config.nest.NEST_OUTPUT_DEVICES_PARAMS_DEF["multimeter"]
+        self.output_devices.append({"model": "multimeter", "params": params,
+                                    "nodes": None, "connections": connections})
