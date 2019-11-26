9e68e48b9ac83ad6c4178eb63315956439142198
Dionysios Perdikis <dionperd@gmail.com>
2019-11-26 19:32:29 +0100

    Removed normalized_weights and other stuff from tvb_scripts Connectivity

--- tvb_scripts/head/model/connectivity.py
+++ tvb_scripts/head/model/connectivity.py
@@ -10,23 +10,6 @@ from tvb.datatypes.connectivity import Connectivity as TVBConnectivity
 
 class Connectivity(TVBConnectivity):
     file_path = ""
-    normalized_weights = np.array([])
-
-    def __init__(self, **kwargs):
-        super(Connectivity, self).__init__(**kwargs)
-        self.file_path = kwargs.pop("file_path", "")
-        self.normalized_weights = kwargs.pop("normalized_weights", np.array([]))
-
-        for attr, value in kwargs.items():
-            try:
-                if len(value):
-                    setattr(self, attr, value)
-            except:
-                warning("Failed to set attribute %s to %s!" % (attr, self.__class__.__name__))
-
-        if len(self.normalized_weights) == 0:
-            self.normalized_weights = normalize_weights(self.weights, remove_diagonal=True, ceil=1.0)
-        self.configure()
 
     @staticmethod
     def from_instance(instance, **kwargs):
@@ -37,35 +20,32 @@ class Connectivity(TVBConnectivity):
                     setattr(result, attr, value)
             except:
                 warning("Failed to set attribute %s to %s!" % (attr, instance.__class__.__name__))
-        if len(result.normalized_weights) == 0:
-            result.normalized_weights = normalize_weights(result.weights, remove_diagonal=True, ceil=1.0)
         result.configure()
         return result
 
-    @staticmethod
-    def from_tvb_instance(instance, **kwargs):
-        result = Connectivity()
-        if isinstance(instance, TVBConnectivity):
-            for attr, value in instance.__dict__:
-                try:
-                    if len(value):
-                        setattr(result, attr, value)
-                except:
-                    warning("Failed to set attribute %s of TVB %s!" % (attr, instance.__class__.__name__))
-        return Connectivity.from_instance(result, **kwargs)
+    # TODO: Fix this!
+    # @staticmethod
+    # def from_tvb_instance(instance, **kwargs):
+    #     result = Connectivity()
+    #     result.file_path = kwargs.pop("file_path", "")
+    #     if isinstance(instance, TVBConnectivity):
+    #         for attr, value in instance.__dict__:
+    #             try:
+    #                 if len(value):
+    #                     setattr(result, attr, value)
+    #             except:
+    #                 warning("Failed to set attribute %s of TVB %s!" % (attr, instance.__class__.__name__))
+    #     return Connectivity.from_instance(result, **kwargs)
 
     @staticmethod
     def from_file(filepath, **kwargs):
         result = TVBConnectivity.from_file(filepath)
         if isinstance(result, TVBConnectivity):
-            return Connectivity.from_tvb_instance(result, **kwargs)
+            raise NotImplementedError
+            # return Connectivity.from_tvb_instance(result, **kwargs)
         else:
+            result.centres = result.centers
             return Connectivity.from_instance(result, **kwargs)
-
-    def configure(self):
-        super(self).configure()
-        if len(self.normalized_weights) == 0:
-            self.normalized_weights = normalize_weights(self.weights, remove_diagonal=True, ceil=1.0)
 
     def regions_labels2inds(self, labels):
         inds = []
@@ -82,9 +62,3 @@ class Connectivity(TVBConnectivity):
     @property
     def centers(self):
         return self.centres
-
-    def __setattr__(self, attr, value):
-        if attr == "centers":
-            setattr(self, "centres", value)
-        else:
-            setattr(self, attr, value)
