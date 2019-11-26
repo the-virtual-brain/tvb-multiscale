110c5c79fc6791e29f0ab9c7618c8fe8f89b3511
Dionysios Perdikis <dionperd@gmail.com>
2019-11-26 18:24:33 +0100

    Onoing work with the example.py

--- tvb_nest/examples/example.py
+++ tvb_nest/examples/example.py
@@ -28,7 +28,7 @@ from tvb.simulator.monitors import Raw  # , Bold  # , EEG
 def main_example(tvb_sim_model, nest_model_builder, tvb_nest_builder, nest_nodes_ids, nest_populations_order=100,
                  connectivity=None, connectivity_zip=CONFIGURED.DEFAULT_CONNECTIVITY_ZIP, simulation_length=100.0,
                  tvb_state_variable_type_label="Synaptic Gating Variable", tvb_state_variables_labels=["S_e", "S_i"],
-                 dt=0.1, exclusive_nodes=False, config=CONFIGURED):
+                 dt=0.1, noise_strength=0.1, exclusive_nodes=False, config=CONFIGURED):
 
     plotter = Plotter(config)
 
@@ -46,6 +46,7 @@ def main_example(tvb_sim_model, nest_model_builder, tvb_nest_builder, nest_nodes
     # We choose all defaults in this example
     simulator = Simulator()
     simulator.integrator.dt = dt
+    simulator.integrator.noise.nsig = np.array([noise_strength])
     simulator.model = tvb_sim_model
 
     simulator.connectivity = connectivity
@@ -108,14 +109,33 @@ if __name__ == "__main__":
     for id in range(connectivity.region_labels.shape[0]):
         if connectivity.region_labels[id].find("hippo") > 0:
             nest_nodes_ids.append(id)
-    model = Generic2dOscillator()
-    model.a = np.array([2.0])
-    model.b = np.array([-10.0])
-    model.c = np.array([0.0])
-    model.d = np.array([0.02])
-    model.I = np.array([0.0])
-    model.tau = np.array([10.0])
-    main_example(model, DefaultExcIOInhIBuilder, InterfaceGeneric2DOscillatorBuilder,
+    # # -----------------------------Generic2dOscillator oscillatory regime-----------------------------------------------
+    # model = Generic2dOscillator()
+    # model.d = np.array([0.1])
+    # model.e = np.array([0.0])
+    # model.f = np.array([1.0])
+    # model.g = np.array([1.0])
+    # model.I = np.array([0.0])
+    # model.tau = np.array([1.0])
+    # model.alpha = np.array([1.0])
+    # model.beta = np.array([0.0])
+    # model.a = np.array([0.0])
+    # model.b = np.array([-1.0])
+    # model.c = np.array([0.0])
+    # -----------------------------------Wilson Cowan oscillatory regime------------------------------------------------
+    model = WilsonCowan()
+    model.tau_e = np.array([8.0])
+    model.tau_i = np.array([8.0])
+    model.c_ee = np.array([16.0])
+    model.c_ei = np.array([12.0])
+    model.c_ie = np.array([15.0])
+    model.c_ii = np.array([3.0])
+    model.a_e = np.array([1.3])
+    model.a_i = np.array([2.0])
+    model.b_e = np.array([4.0])
+    model.b_i = np.array([3.7])
+    model.P = np.array([0.0])
+    main_example(model, DefaultExcIOInhIBuilder, InterfaceWilsonCowanBuilder,
                  nest_nodes_ids, nest_populations_order=100, connectivity=connectivity, simulation_length=500.0,
-                 tvb_state_variable_type_label="State Variables", tvb_state_variables_labels=["V", "W"],
-                 exclusive_nodes=True, config=CONFIGURED)
+                 tvb_state_variable_type_label="State Variables", tvb_state_variables_labels=["E", "I"],
+                 exclusive_nodes=True, noise_strength=0.000001, config=CONFIGURED)
