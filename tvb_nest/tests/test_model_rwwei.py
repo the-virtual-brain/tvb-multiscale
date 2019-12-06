603d2b5c63a0b50a350c0beeaaff426fb64540c0
Dionysios Perdikis <dionperd@gmail.com>
2019-12-06 15:13:32 +0100

    Updated test_model_rwwei for the new model that computes rates as part of the state

--- tvb_nest/tests/test_model_rwwei.py
+++ tvb_nest/tests/test_model_rwwei.py
@@ -11,24 +11,32 @@ octave.addpath(os.path.join(TESTS_PATH, "DMF2014"))
 
 
 def reduced_wong_wang_exc_io_inh_i(N, abs_err, tvb_model, lamda):
-    exc = 1.0 * np.random.uniform(size=(N, 1))
-    inh = 1.0 * np.random.uniform(size=(N, 1))
+    Sexc = 1.0 * np.random.uniform(size=(N, 1))
+    Sinh = 1.0 * np.random.uniform(size=(N, 1))
+    state_matlab = np.array([Sexc, Sinh])
 
-    state = np.array([exc, inh])
-    coupling = exc[np.newaxis]
+    if isinstance(tvb_model, ReducedWongWangExcIOInhI):
+        Rexc = 1000.0 * np.random.uniform(size=(N, 1))
+        Rinh = 1000.0 * np.random.uniform(size=(N, 1))
+        state_python = np.array([Sexc, Sinh, Rexc, Rinh])
+        get_S = lambda state: state[:2]
+    else:
+        state_python = state_matlab
+        get_S = lambda state: state
+    coupling = Sexc[np.newaxis]
 
-    matlab_output = octave.matlab_rww_dfun(exc, inh, exc, lamda)
+    matlab_output = octave.matlab_rww_dfun(Sexc, Sinh, Sexc, lamda)
     matlab_dstate = np.array([matlab_output[0, 0], matlab_output[0, 1]]).squeeze()
 
-    numpy_dstate = tvb_model._numpy_dfun(state, coupling).squeeze()
-    max_difference = np.max(np.abs(numpy_dstate - matlab_dstate))
+    numpy_dstate = tvb_model._numpy_dfun(state_python, coupling).squeeze()
+    max_difference = np.max(np.abs(get_S(numpy_dstate) - matlab_dstate))
     assert matlab_dstate.shape == (2, N)
     assert max_difference < abs_err
 
-    numba_dstate = tvb_model.dfun(state, coupling).squeeze()
+    numba_dstate = tvb_model.dfun(state_python, coupling).squeeze()
     max_difference = np.max(np.abs(numpy_dstate - numba_dstate))
     assert max_difference < abs_err
-    max_difference = np.max(np.abs(numba_dstate - matlab_dstate))
+    max_difference = np.max(np.abs(get_S(numba_dstate) - matlab_dstate))
     assert max_difference < abs_err
 
 
