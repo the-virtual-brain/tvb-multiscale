d0a3be94434d6bac7f0e0ef3460162dd9f99b2c4
Dionysios Perdikis <dionperd@gmail.com>
2019-12-13 21:26:44 +0100

    New red_ww_exc_io_inh_i and red_ww_exc_io_inh_i_multisynapse models and interfaces

--- tvb_nest/interfaces/builders/models/red_ww_exc_io_inh_i.py
+++ tvb_nest/interfaces/builders/models/red_ww_exc_io_inh_i.py
@@ -15,8 +15,8 @@ class RedWWexcIOinhIBuilder(TVBNESTInterfaceBuilder):
 
         if tvb_to_nest_interfaces is None:
     # # For directly setting an external current parameter in NEST neurons instantaneously:
-    #       interface_weight_fun = lambda nest_node_id=None: \
-    #                                       np.maximum(1.0, 5.0 + 2.0*np.random.normal())
+    #         interface_weight_fun = lambda nest_node_id=None: \
+    #                                       np.maximum(1.0, 1.0 + 0.3*np.random.normal())
     #         tvb_to_nest_interfaces = [{"model": "current",  "parameter": "I_e",
     # # ---------Properties potentially set as function handles with args (nest_node_id=None)---------------------------
     #                                    "interface_weights": interface_weight_fun(),
@@ -25,13 +25,12 @@ class RedWWexcIOinhIBuilder(TVBNESTInterfaceBuilder):
     #                                    "connections": {"S_e": ["E", "I"]},
     #                                    "nodes": None}]  # None means all here
 
-            interface_weight_fun = lambda tvb_node_id=None, nest_node_id=None: \
-                np.maximum(1.0, tvb_simulator.model.G[0] * (1.0 + 0.3*np.random.normal()))
-
     # # For injecting current to NEST neurons via dc generators acting as TVB proxy nodes with TVB delays:
+    #         interface_weight_fun = lambda tvb_node_id=None, nest_node_id=None: \
+    #             20 * np.maximum(1.0, tvb_simulator.model.G[0] * (1.0 + 0.3 * np.random.normal()))
     #         tvb_to_nest_interfaces = [{"model": "dc_generator", "params": {},
     # # -------Properties potentially set as function handles with args (tvb_node_id=None, nest_node_id=None)-----------
-    #                                    "interface_weights": 100.0,  # Applied outside NEST for each interface device
+    #                                    "interface_weights": 1.0,  # Applied outside NEST for each interface device
     #                                    "weights": interface_weight_fun(),  # To multiply TVB connectivity weight
     # #                                 To add to TVB connectivity delay:
     # #                                   "delays": nest_network.nodes_min_delay,
@@ -47,10 +46,12 @@ class RedWWexcIOinhIBuilder(TVBNESTInterfaceBuilder):
     # "model": "mip_generator", "params": {"p_copy": 0.5, "mother_seed": 0}
     # An alternative option to poisson_generator is:
     # "model": "inhomogeneous_poisson_generator", "params": {"allow_offgrid_times": False}
+            interface_weight_fun = lambda tvb_node_id=None, nest_node_id=None: \
+                100 * np.maximum(1.0, tvb_simulator.model.G[0] * (1.0 + 0.3 * np.random.normal()))
             tvb_to_nest_interfaces = [{"model": "poisson_generator", "params": {},
     # -------Properties potentially set as function handles with args (tvb_node_id=None, nest_node_id=None)-----------
-                                        "interface_weights": 200.0,  # Applied outside NEST for each interface device
-                                        "weights": interface_weight_fun(),  # To multiply TVB connectivity weight
+                                        "interface_weights": 1.0,  # Applied outside NEST for each interface device
+                                        "weights": interface_weight_fun,  # To multiply TVB connectivity weight
     #                                 To add to TVB connectivity delay:
                                         "delays": nest_network.nodes_min_delay,
                                         "receptor_types": 0,
@@ -77,10 +78,11 @@ class RedWWexcIOinhIBuilder(TVBNESTInterfaceBuilder):
 
         super(RedWWexcIOinhIBuilder, self).__init__(tvb_simulator, nest_network, nest_nodes_ids, exclusive_nodes,
                                                     tvb_to_nest_interfaces, nest_to_tvb_interfaces)
-        self.w_tvb_to_current = 1000 * self.tvb_model.J_N  # (nA of TVB -> pA of NEST)
-        self.w_tvb_to_spike_rate = 1.0  # r parameter is in the order of 1000 Hz for WongWang model
-        # (assuming spikes/ms in TVB) and given the division of spikes' number with dt in ms:
-        self._spikes_to_tvb_rate = 1.0
+        self.w_tvb_to_current = 1000 * self.tvb_model.J_N[0]  # (nA of TVB -> pA of NEST)
+        # WongWang model parameter r is in Hz, just like poisson_generator assumes in NEST:
+        self.w_tvb_to_spike_rate = 1.0
+        # given WongWang model parameter r is in Hz but dt is in ms:
+        self.w_spikes_to_tvb = 1000.0
 
     def build_interface(self, tvb_nest_interface=None):
         if not isinstance(tvb_nest_interface, RedWWexcIOinhI):
--- tvb_nest/simulator_nest/builders/models/red_ww_exc_io_inh_i_multisynapse.py
+++ tvb_nest/simulator_nest/builders/models/red_ww_exc_io_inh_i_multisynapse.py
@@ -6,32 +6,34 @@ from tvb_nest.config import CONFIGURED
 from tvb_nest.simulator_nest.builders.base import NESTModelBuilder
 
 
-class RedWWExcIOInhIBuilder(NESTModelBuilder):
+class RedWWExcIOInhIMultisynapseBuilder(NESTModelBuilder):
 
     def __init__(self, tvb_simulator, nest_nodes_ids, nest_instance=None, config=CONFIGURED,
-                 V_th=-50.0,  # mV
-                 V_reset=-55.0,  # mV
-                 E_L=-70.0,  # mV
+                 V_th=-50.0,            # mV
+                 V_reset=-55.0,         # mV
+                 E_L=-70.0,             # mV
+                 tau_w=1000.0,          # ms a very large tau_w to practically eliminate spike adaptation current
                  # exc neurons (AMPA,rec/ext, NMDA)
-                 C_m_ex=500.0,  # pF
-                 g_L_ex=25.0,  # nS
-                 t_ref_ex=2.0,  # ms
+                 C_m_ex=500.0,          # pF
+                 g_L_ex=25.0,           # nS
+                 t_ref_ex=2.0,          # ms
                  # inh neurons (GABA):
-                 C_m_in=200.0,  # pF
-                 g_L_in=20.0,  # nS
-                 t_ref_in=1.0,  # ms
+                 C_m_in=200.0,          # pF
+                 g_L_in=20.0,           # nS
+                 t_ref_in=1.0,          # ms
                  # exc spikes (AMPA,rec/ext, NMDA):
-                 E_ex=0.0,  # mV
-                 tau_decay_ex=100.0,  # maximum(AMPA,rec, NMDA) = maximum(2.0, 100.0) ms
-                 tau_rise_ex=2.0,  # tau_rise_NMDA = 2.0 ms
+                 E_ex=0.0,              # mV
+                 tau_rise_ex=2.0,      # tau_rise_NMDA =2.0 ms
+                 tau_decay_ex=100.0,   # maximum(AMPA,rec, NMDA) = maximum(2.0, 100.0) ms
                  # ext, exc spikes(AMPA, ext):
+                 tau_decay_ex_ext=2.0,       # ms
                  # inh spikes (GABA):
-                 E_in=-70.0,  # mV
-                 tau_decay_in=10.0,  # tau_GABA = 10.0 ms
-                 tau_rise_in=1.0  # assuming tau_rise_GABA = 1.0 ms
+                 E_in=-70.0,            # mV
+                 tau_rise_in=1.0,   # assuming tau_rise_GABA = 1.0 ms
+                 tau_decay_in=10.0  # tau_decay_GABA =10.0 ms
                  ):
-        # config.nest.DEFAULT_MODEL = "iaf_cond_deco2014"
-        super(RedWWExcIOInhIBuilder, self).__init__(tvb_simulator, nest_nodes_ids, nest_instance, config)
+        config.nest.DEFAULT_MODEL = "aeif_cond_beta_multisynapse"
+        super(RedWWExcIOInhIMultisynapseBuilder, self).__init__(tvb_simulator, nest_nodes_ids, nest_instance, config)
 
         # Common order of neurons' number per population:
         self.populations_order = 100
@@ -41,21 +43,22 @@ class RedWWExcIOInhIBuilder(NESTModelBuilder):
         # set a handle to a function with
         # arguments (region_index=None) returning the corresponding property
 
+        E_rev = np.array([E_ex] +  # exc spikes (AMPA,rec, NMDA)
+                         [E_in] +  # inh spikes (GABA)
+                         self.number_of_nodes * [E_ex])  # ext, exc spikes (AMPA,ext)
+        tau_rise = np.array([tau_rise_ex] +  # exc spikes (AMPA,rec, NMDA)
+                            [tau_rise_in] +  # inh spikes (GABA)
+                           self.number_of_nodes * [tau_rise_ex])  # ext, exc spikes (AMPA,ext)
+        tau_decay = np.array([tau_decay_ex] +  # exc spikes (AMPA,rec, NMDA)
+                            [tau_decay_in] +  # inh spikes (GABA)
+                            self.number_of_nodes * [tau_decay_ex_ext])  # ext, exc spikes (AMPA,ext)
         common_params = {
-            "V_th": V_th, "V_reset": V_reset, "E_L": E_L,
-            "E_ex": E_ex, "E_in": E_in,
-            "tau_rise_ex": tau_rise_ex, "tau_rise_in": tau_rise_in,
-            "tau_decay_ex": tau_decay_ex, "tau_decay_in": tau_decay_in,
-        }
-
+            "V_th": V_th, "V_peak": 0.0, "V_reset": V_reset, "E_L": E_L, "E_rev": E_rev,
+            "tau_w": tau_w, "tau_rise": tau_rise, "tau_decay": tau_decay}
         self.params_ex = dict(common_params)
-        self.params_ex.update({
-            "C_m": C_m_ex, "g_L": g_L_ex, "t_ref": t_ref_ex,
-        })
+        self.params_ex.update({"C_m": C_m_ex, "g_L": g_L_ex, "t_ref": t_ref_ex})
         self.params_in = dict(common_params)
-        self.params_in.update({
-            "C_m": C_m_in, "g_L": g_L_in, "t_ref": t_ref_in,
-        })
+        self.params_in.update({"C_m": C_m_in, "g_L": g_L_in, "t_ref": t_ref_in})
         self.populations = [{"label": "E", "model": self.default_population["model"],
                              "nodes": None,  # None means "all"
                              "params": self.params_ex,
@@ -76,32 +79,32 @@ class RedWWExcIOInhIBuilder(NESTModelBuilder):
              "conn_spec": self.default_populations_connection["conn_spec"],
              "weight": self.tvb_model.w_p[0],
              "delay": self.default_populations_connection["delay"],
-             "receptor_type": 0, "nodes": None},  # None means "all"
+             "receptor_type": 1, "nodes": None},  # None means "all"
             {"source": "E", "target": "I",  # E -> I
+             "model": self.default_populations_connection["model"],
+             "conn_spec": self.default_populations_connection["conn_spec"],
+             "weight": 1.0, "delay": self.default_populations_connection["delay"],
+             "receptor_type": 1, "nodes": None},  # None means "all"
+            {"source": "I", "target": "E",  # I -> E
+             "model": self.default_populations_connection["model"],
+             "conn_spec": self.default_populations_connection["conn_spec"],
+             "weight": self.tvb_model.J_i[0],
+             "delay": self.default_populations_connection["delay"],
+             "receptor_type": 2, "nodes": None},  # None means "all"
+            {"source": "I", "target": "I",  # I -> I This is a self-connection for population "I"
              "model": self.default_populations_connection["model"],
              "conn_spec": self.default_populations_connection["conn_spec"],
              "weight": 1.0,
              "delay": self.default_populations_connection["delay"],
-             "receptor_type": 0, "nodes": None},  # None means "all"
-            {"source": "I", "target": "E",  # I -> E
-             "model": self.default_populations_connection["model"],
-             "conn_spec": self.default_populations_connection["conn_spec"],
-             "weight": -self.tvb_model.J_i[0],
-             "delay": self.default_populations_connection["delay"],
-             "receptor_type": 0, "nodes": None},  # None means "all"
-            {"source": "I", "target": "I",  # I -> I This is a self-connection for population "I"
-             "model": self.default_populations_connection["model"],
-             "conn_spec": self.default_populations_connection["conn_spec"],
-             "weight": -1.0,
-             "delay": self.default_populations_connection["delay"],
-             "receptor_type": 0, "nodes": None}  # None means "all"
+             "receptor_type": 2, "nodes": None}  # None means "all"
         ]
+
         # Among/Between region-node connections
         # Given that only the AMPA population of one region-node couples to
         # all populations of another region-node,
         # we need only one connection type
         nodes_weight_fun = lambda source_nest_node_id=None, target_nest_node_id=None: \
-            100 * np.maximum(1.0, self.tvb_simulator.model.G[0] * (1.0 + 0.1 * np.random.normal()))
+            20 * np.maximum(1.0, self.tvb_simulator.model.G[0] * (1.0 + 0.1 * np.random.normal()))
         self.nodes_connections = [
             {"source": "E", "target": ["E", "I"],
              "model": self.default_nodes_connection["model"],
@@ -109,7 +112,8 @@ class RedWWExcIOInhIBuilder(NESTModelBuilder):
              "weight": nodes_weight_fun,  # weight scaling the TVB connectivity weight
              "delay": self.default_nodes_connection["delay"],  # additional delay to the one of TVB connectivity
              # Each region emits spikes in its own port:
-             "receptor_type": 0,  "source_nodes": None, "target_nodes": None}  # None means "all"
+             "receptor_type": lambda source_region_index, target_region_index=None: int(source_region_index+3),
+             "source_nodes": None, "target_nodes": None}  # None means "all"
                                  ]
 
         # Creating  devices to be able to observe NEST activity:
