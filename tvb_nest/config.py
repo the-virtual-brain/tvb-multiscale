72b7dc7332122dede166403960b70b08ddac0a8c
liadomide <lia_domide@yahoo.com>
2020-02-02 21:58:27 +0200

    Fix after merge

--- tvb_nest/config.py
+++ tvb_nest/config.py
@@ -10,7 +10,7 @@ MODULES_DIR = os.path.join(TVB_NEST_DIR, "tvb_nest/nest/modules")
 MODULES_BLDS_DIR = os.path.join(TVB_NEST_DIR, "tvb_nest/nest/modules_builds")
 
 
-class NESTconfig(object):
+class Config(ConfigBase):
     # WORKING DIRECTORY:
     TVB_NEST_DIR = TVB_NEST_DIR
     WORKING_DIR = WORKING_DIR
@@ -40,9 +40,11 @@ class NESTconfig(object):
                                      "mip_generator": {"p_copy": 0.5, "mother_seed": 0},
                                      "inhomogeneous_poisson_generator": {"allow_offgrid_times": False}}
 
-    def __init__(self, nest_path=os.environ["NEST_INSTALL_DIR"], nest_python=os.environ["NEST_PYTHON_PREFIX"]):
-        self.NEST_PATH = nest_path
-        self.PYTHON = nest_python
+    def __init__(self, head_folder=WORKING_DIR, raw_data_folder=DEFAULT_SUBJECT_PATH,
+                 output_base=WORKING_DIR, separate_by_run=False):
+        super(Config, self).__init__(head_folder, raw_data_folder, output_base, separate_by_run)
+        self.NEST_PATH = os.environ["NEST_INSTALL_DIR"]
+        self.PYTHON = os.environ["NEST_PYTHON_PREFIX"]
         self.DATA_DIR = os.path.join(self.NEST_PATH, "share/nest")
         self.SLI_PATH = os.path.join(self.DATA_DIR, "sli")
         self.DOC_DIR = os.path.join(self.NEST_PATH, "share/doc/nest")
@@ -51,14 +53,6 @@ class NESTconfig(object):
         self.WORKING_DIR = WORKING_DIR
         self.MODULES_DIR = MODULES_DIR
         self.MODULES_BLDS_DIR = MODULES_BLDS_DIR
-
-
-class Config(ConfigBase):
-
-    def __init__(self, head_folder=WORKING_DIR, raw_data_folder=DEFAULT_SUBJECT_PATH,
-                 output_base=WORKING_DIR, separate_by_run=False):
-        super(Config, self).__init__(head_folder, raw_data_folder, output_base, separate_by_run)
-        self.NEST_PATH = os.environ.get("NEST_INSTALL_DIR")
 
 
 CONFIGURED = Config()
