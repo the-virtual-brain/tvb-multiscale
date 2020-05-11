2af94092d1f5359ff9a8e3d0cb858a0fd4499eb3
dionperd <dionperd@gmail.com>
2020-05-11 19:21:46 +0200

    Added tvb_utils.utils and moved initialize_logger there

--- tvb_multiscale/config.py
+++ tvb_multiscale/config.py
@@ -5,8 +5,6 @@ import os
 import numpy
 import inspect
 from datetime import datetime
-import logging
-from logging.handlers import TimedRotatingFileHandler
 
 from tvb.basic.profile import TvbProfile
 
@@ -15,6 +13,8 @@ TvbProfile.set_profile(TvbProfile.LIBRARY_PROFILE)
 from tvb.datatypes import cortex, connectivity
 
 import tvb_data
+
+from tvb_utils.utils import initialize_logger as initialize_logger_base
 
 
 TVB_NEST_DIR = os.path.abspath(__file__).split("tvb_multiscale")[0]
@@ -227,34 +227,4 @@ CONFIGURED = Config()
 
 
 def initialize_logger(name, target_folder=CONFIGURED.out.FOLDER_LOGS):
-    """
-    create logger for a given module
-    :param name: Logger Base Name
-    :param target_folder: Folder where log files will be written
-    """
-    if not (os.path.isdir(target_folder)):
-        os.makedirs(target_folder)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
-
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setFormatter(formatter)
-    ch.setLevel(logging.DEBUG)
-
-    fh = TimedRotatingFileHandler(os.path.join(target_folder, 'logs.log'), when="d", interval=1, backupCount=2)
-    fh.setFormatter(formatter)
-    fh.setLevel(logging.DEBUG)
-
-    # Log errors separately, to have them easy to inspect
-    fhe = TimedRotatingFileHandler(os.path.join(target_folder, 'log_errors.log'), when="d", interval=1, backupCount=2)
-    fhe.setFormatter(formatter)
-    fhe.setLevel(logging.ERROR)
-
-    logger.addHandler(ch)
-    logger.addHandler(fh)
-    logger.addHandler(fhe)
-
-    return logger
+    return initialize_logger_base(name, target_folder)
