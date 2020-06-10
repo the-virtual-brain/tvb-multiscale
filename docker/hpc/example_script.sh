6845f5e67bc4c5c27e9d930edd6afa30d1586429
dionperd <dionperd@gmail.com>
2020-06-10 19:08:44 +0200

    Corrected singularity commands in example_script.sh

--- docker/hpc/example_script.sh
+++ docker/hpc/example_script.sh
@@ -10,7 +10,6 @@ export DOCKER_HOME=/home/docker
 export DOCKER_TVB_ROOT=$HOME_DOCKER/packages/tvb-root
 export DOCKER_TVB_MULTISCALE=$HOME_DOCKER/packages/tvb-multiscale
 
-export DOCKER_IMAGE='thevirtualbrain/tvb-nest:dp-ongoing-work'
 export CMD="/home/docker/env/neurosci/bin/python /home/docker/packages/tvb-multiscale/tvb_nest/examples/example.py"
 
 export NODES=mc  # or gpu, nothing for BIH cluster
@@ -20,6 +19,7 @@ export SRUN='srun -C $NODES'
 ## In PizDaint, we assume that these lines have been already ran:
 #module load daint-mc # or daint-gpu
 #module load sarus
+export DOCKER_IMAGE='thevirtualbrain/tvb-nest:dp-ongoing-work'
 
 export INTERACTIVE_CMD='sarus --debug run -t
                         --mount=type=bind,source=${TVB_MULTISCALE},destination=${DOCKER_TVB_MULTISCALE}
@@ -39,18 +39,22 @@ export SRUN_COMMAND='$SRUN $COMMAND'
 ## In PizDaint, we assume that these lines have been already ran:
 #module load daint-mc # or daint-gpu
 #module load singularity/3.5.3-daint
+# singularity pull docker://${DOCKER_IMAGE}
+export DOCKER_IMAGE_RUN='tvb-nest_dp-ongoing-work.sif'
+
+# Run from within the path where the .sif file is:
 
 export INTERACTIVE_CMD='singularity shell
-                        --bind ${TVB_MULTISCALE}:${DOCKER_TVB_MULTISCALE}
-                        --bind ${TVB_ROOT}:${DOCKER_TVB_ROOT}
-                        docker://${DOCKER_IMAGE}'
+                        --bind $TVB_MULTISCALE:$DOCKER_TVB_MULTISCALE
+                        --bind $TVB_ROOT:${DOCKER_TVB_ROOT}
+                        $DOCKER_IMAGE_RUN'
 
 export SRUN_INTERACTIVE_CMD='$SRUN -pty -C ${INTERACTIVE_CMD}'
 
 export COMMAND='singularity exec
-                --bind ${TVB_MULTISCALE}:${DOCKER_TVB_MULTISCALE}
-                --bind ${TVB_ROOT}:${DOCKER_TVB_ROOT}
-                docker://${DOCKER_IMAGE} $CMD'
+                --bind $TVB_MULTISCALE:$DOCKER_TVB_MULTISCALE
+                --bind $TVB_ROOT:$DOCKER_TVB_ROOT
+                $DOCKER_IMAGE $CMD'
 
 export SRUN_COMMAND='$SRUN $COMMAND'
 
