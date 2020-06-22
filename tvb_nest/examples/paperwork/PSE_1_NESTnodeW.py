f2da2e3b4061da54fc48d5540fe3fb4adc7aafa8
dionperd <dionperd@gmail.com>
2020-06-22 17:03:33 +0200

    Adding script for cluster PSE with St and w params with 1 simulation per job

---
+++ tvb_nest/examples/paperwork/PSE_1_NESTnodeW.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import sys
+import time
+from tvb_nest.examples.paperwork.paperwork_pse_exc_io import single_nest_PSE
+from tvb_nest.examples.paperwork.paperwork_pse_exc_io import print_toc_message
+
+
+args = sys.argv
+
+tic = time.time()
+
+try:
+    fast = args[3] == "fast"
+except:
+    fast = False
+
+print("fast=%s" % str(fast))
+
+# Run PSE for default St values:
+single_nest_PSE(w=float(args[1]), branch=args[2], fast=fast)
+
+print_toc_message(tic)
