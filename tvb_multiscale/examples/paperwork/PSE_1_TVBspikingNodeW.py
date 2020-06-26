9100cbc273fae5960281f314b341878d87e02d7e
dionperd <dionperd@gmail.com>
2020-06-26 20:14:47 +0200

    Cluster functions-scripts for spiking TVB workflows

---
+++ tvb_multiscale/examples/paperwork/PSE_1_TVBspikingNodeW.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import sys
+import time
+from tvb_multiscale.examples.paperwork.paperwork_pse_exc_io import single_spiking_PSE
+from tvb.contrib.scripts.utils.log_error_utils import print_toc_message
+
+args = sys.argv
+
+tic = time.time()
+
+try:
+    fast = args[4] == "fast"
+except:
+    fast = False
+
+single_spiking_PSE(w=float(args[1]), branch=args[2], fast=fast, output_base=args[3])
+
+print_toc_message(tic)
