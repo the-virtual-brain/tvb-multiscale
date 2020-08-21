78a48e54a1ee5b5f841f616f8a0eea9326c3ec54
dionperd <dionperd@gmail.com>
2020-08-21 12:25:28 +0200

    Updated modules to be compiled for testing

--- docker/module_build_test.py
+++ docker/module_build_test.py
@@ -2,8 +2,5 @@
 
 import nest
 
-nest.Install("tvb_rate_wongwangmodule")
-nest.Install("iaf_cond_deco2014module")
-nest.Install("iaf_cond_nmda_deco2014module")
-nest.Install("iaf_cond_ampa_gaba_deco2014module")
-nest.Install("scale_connectionmodule")
+nest.Install("iaf_cond_ww_decomodule")
+nest.Install("izhikevich_hamkermodule")
