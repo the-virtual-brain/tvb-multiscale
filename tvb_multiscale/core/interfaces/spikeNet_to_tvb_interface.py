4d6779dce8121b4f659049be80bc701781318e44
dionperd <dionperd@gmail.com>
2020-09-15 18:31:19 +0200

    Updated/Corrected spikeNet_to_tvb_interface.py. Not using do_for_devices method. Added number_of_events property. Speeding it up?

--- tvb_multiscale/core/interfaces/spikeNet_to_tvb_interface.py
+++ tvb_multiscale/core/interfaces/spikeNet_to_tvb_interface.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from six import string_types
 
-from pandas import Series, unique
-from numpy import array
+import numpy as np
+from pandas import unique
 
 from tvb_multiscale.core.config import initialize_logger
 from tvb_multiscale.core.spiking_models.devices import DeviceSet
@@ -20,13 +20,14 @@ class SpikeNetToTVBinterface(DeviceSet):
     # via output/measuring devices
 
     def __init__(self, spiking_network, tvb_sv_id, name="", model="",
-                 nodes_ids=[], scale=array([1.0]), device_set=None):
+                 nodes_ids=[], scale=np.array([1.0]), device_set=None):
         super(SpikeNetToTVBinterface, self).__init__(name, model, device_set)
         self.spiking_network = spiking_network
         self.tvb_sv_id = tvb_sv_id  # The index of the TVB state variable linked to this interface
         # The indices of the Spiking Nodes which coincide with the TVB region nodes
         # (i.e., region i implemented in Spiking Network updates the region i in TVB):
         self.nodes_ids = nodes_ids
+        self.number_of_events = np.zeros((len(self.nodes_ids),))
         self.scale = scale  # a scaling weight
         LOG.info("%s of model %s for %s created!" % (self.__class__, self.model, self.name))
 
@@ -59,18 +60,46 @@ class SpikeNetToTVBinterface(DeviceSet):
 
     @property
     def population_mean_spikes_number(self):
-        values = array(self.do_for_all_devices("mean_number_of_spikes")).flatten()
-        self.reset
-        return values
+        values = []
+        for i_node, (node, n_events) in enumerate(zip(self.devices(), self.number_of_events)):
+            number_of_events = self[node].number_of_events
+            if number_of_events > n_events:
+                values.append((number_of_events-n_events)/self[node].number_of_neurons)
+            else:
+                values.append(0.0)
+            self.number_of_events[i_node] = number_of_events
+        return np.array(values).flatten()
 
     @property
     def population_mean_spikes_activity(self):
-        return array(self.do_for_all_devices("mean_spikes_activity")).flatten()
+        values = []
+        for i_node, (node, n_events) in enumerate(zip(self.devices(), self.number_of_events)):
+            number_of_events = self[node].number_of_events
+            if number_of_events > n_events:
+                # if there are new events, sum up only the spikes' weights not yet considered:
+                values.append(np.sum(self.events[self.spike_var][n_events:]) / self[node].number_of_neurons)
+            else:
+                values.append(0.0)
+            self.number_of_events[i_node] = number_of_events
+        return np.array(values).flatten()
 
     @property
     def current_population_mean_values(self):
-        return array(self.do_for_all_devices("current_data_mean_values")).flatten()
-
-    @property
-    def reset(self):
-        return array(self.do_for_all_devices("reset")).flatten()
+        values = []
+        for i_node, (node, n_events) in enumerate(zip(self.devices(), self.number_of_events)):
+            number_of_events = self[node].number_of_events
+            if number_of_events > n_events:
+                # if there are new events, take the mean of all the values not yet considered:
+                # (unlike discrete spike events, for continuous multimeter events
+                # we assume that the new events are of size n_time_steps * n_neurons,
+                # and, unlike spike weights' time series,
+                # we compute mean absolute activity and not a rate
+                # (i.e., with division by number of time points instead of time)
+                this_node_values = []
+                for var in self[node].record_from:
+                    this_node_values.append(np.mean(self.events[var][n_events:]))
+                values.append(np.array(this_node_values))
+            else:
+                values.append(np.zeros((len(self.record_from),)))
+            self.number_of_events[i_node] = number_of_events
+        return np.array(values).flatten()
