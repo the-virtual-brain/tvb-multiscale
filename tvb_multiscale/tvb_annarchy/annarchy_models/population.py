1d65358512582b7b45a4215d07a4829452f762af
Fox8888 <33659903+Fox8888@users.noreply.github.com>
2020-09-18 17:32:58 +0200

    Created a connect() method in population.py that sets up a projection and connects it. To do: Getting connections to/from an arbitrary group of neurons inside the population.

--- tvb_multiscale/tvb_annarchy/annarchy_models/population.py
+++ tvb_multiscale/tvb_annarchy/annarchy_models/population.py
@@ -11,16 +11,15 @@ from tvb.contrib.scripts.utils.data_structures_utils import ensure_list, list_of
 class ANNarchyPopulation(SpikingPopulation):
 
     annarchy_instance = None
-    population_neurons = None # A population object
-    projections_pre = []
-    projections_post = []
+    projections_in = []  # incoming projections
+    projections_out = []  # outgoing projections
     _weight_attr = "weight"
     _delay_attr = "delay"
     _receptor_attr = "receptor"
 
     def __init__(self, population_neurons, label="", model="", annarchy_instance=None, **kwargs):
         self.annarchy_instance = annarchy_instance
-        self.population_neurons = population_neurons
+        self._population = population_neurons
         super(ANNarchyPopulation, self).__init__(label, model, **kwargs)
 
     @property
@@ -34,7 +33,24 @@ class ANNarchyPopulation(SpikingPopulation):
             tuple of neurons'indices.
             In ANNarchy: So far we get only local indices.
         """
-        return tuple(self.population_neurons.ranks)
+        return tuple(self._population.ranks)
+
+    def connect(self, target_pop, target, synapse=None, name=None,
+                method="connect_all_to_all", **connection_args):
+        """
+        Method to set up and connect a projection between two ANNarchyPopulations.
+        Arguments:
+            target_pop: The ANNarchyPopulation we want to connect to.
+            target: type of the connection. Needs to be set, or weights are zero.
+            synapse: a ``Synapse`` instance.
+            name: name of the projection
+            method: name of an ANNarchy connection method
+            **connection_args: depend on the chosen ANNarchy connection method
+        """
+        proj = self.annarchy_instance.Projection(self._population, target_pop._population, target, synapse, name)
+        self.projections_in.append(proj)  # is there a faster way than using .append()?
+        target_pop.projections_out.append(proj)
+        return getattr(proj, method)(**connection_args)
 
     def _assert_annarchy(self):
         if self.annarchy_instance is None:
@@ -45,7 +61,7 @@ class ANNarchyPopulation(SpikingPopulation):
         """ Prints indices of neurons in this population.
             Currently we get only local indices.
         """
-        return "\n%s" % str(self.population_neurons.ranks)
+        return "\n%s" % str(self._population.ranks)
 
     def _Set(self, neurons, values_dict):
         """Method to set attributes of the SpikingPopulation's neurons.
@@ -54,7 +70,7 @@ class ANNarchyPopulation(SpikingPopulation):
             values_dict: dictionary of attributes names' and values.
         """
         self._assert_annarchy()
-        self.population_neurons[neurons].set(values_dict)
+        self._population[neurons].set(values_dict)
 
     def _Get(self, neurons, attrs=None):
         """Method to get attributes of the SpikingPopulation's neurons.
@@ -68,41 +84,37 @@ class ANNarchyPopulation(SpikingPopulation):
         dictionary = {}
         if attrs is None:
             # If no attribute is specified, return all of them
-            for attribute in self.population_neurons[neurons].attributes:
-                dictionary[attribute] = self.population_neurons[neurons].get(attribute).tolist()
+            for attribute in self._population[neurons].attributes:
+                dictionary[attribute] = self._population[neurons].get(attribute).tolist()
         else:
             for attribute in attrs:
-                dictionary[attribute] = self.population_neurons[neurons].get(attribute).tolist()
+                dictionary[attribute] = self._population[neurons].get(attribute).tolist()
         return dictionary
 
     def _GetConnections(self, neurons=None, source_or_target=None):
         """Method to get all the connections from/to a SpikingPopulation neuron.
         Arguments:
-            neurons: tuple of neurons the connections of which should be included in the output.
+            neurons: not implemented yet.
+                     was in nest: nest.NodeCollection or sequence (tuple, list, array) of neurons
+                     the connections of which should be included in the output.
             source_or_target: Direction of connections relative to the populations' neurons
                               "source", "target" or None (Default; corresponds to both source and target)
            Returns:
-            synapses' collections.
+            A list of projections.
         """
         self._assert_annarchy()
-        if neurons is not None:
-            if len(neurons) == 0:
-                neurons = None
-        else:
-            neurons = self.population_neurons.ranks
-        if neurons is not None and not isinstance(neurons, self.annarchy_instance.NodeCollection):
-            neurons = self.annarchy_instance.NodeCollection(neurons)
+        # neurons = self._assert_neurons(neurons)
         if source_or_target not in ["source", "target"]:
-            return self.annarchy_instance.GetConnections(source=neurons), \
-                   self.annarchy_instance.GetConnections(target=neurons)
-        else:
-            kwargs = {source_or_target: neurons}
-            return self.annarchy_instance.GetConnections(**kwargs)
+            return self.projections_in, self.projections_out
+        elif source_or_target == "source":
+            return self.projections_out
+        elif source_or_target == "target":
+            return self.projections_in
 
     def _SetToConnections(self, connections, values_dict):
         """Method to set attributes of the connections from/to the SpikingPopulation's neurons.
            Arguments:
-             connections: connections' objects.
+             connections: a projection object.
              values_dict: dictionary of attributes names' and values.
         """
         self._assert_annarchy()
