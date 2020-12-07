92131dcacd2a11b75783831e7b2536ab113dd1aa
dionperd <dionperd@gmail.com>
2020-12-07 18:34:02 +0100

    Added kwargs to correlations_plot.py

--- tvb_multiscale/core/plot/correlations_plot.py
+++ tvb_multiscale/core/plot/correlations_plot.py
@@ -3,13 +3,15 @@ from matplotlib import pyplot
 from tvb_multiscale.core.utils.data_structures_utils import combine_DataArray_dims
 
 
-def plot_correlations(corrs, plotter):
+def plot_correlations(corrs, plotter, **kwargs):
     from xarray import DataArray
     data, dims, coords = combine_DataArray_dims(corrs, [(0, 2), (1, 3)], join_string=", ", return_array=False)
+    figsize = kwargs.pop("figsize", plotter.config.DEFAULT_SIZE)
     pyplot.figure()
     DataArray(data, dims=dims, name=corrs.name). \
         plot(x=dims[0], y=dims[1], cmap="jet",
-             xticks=np.arange(data.shape[0]), yticks=np.arange(data.shape[1]))
+             xticks=np.arange(data.shape[0]), yticks=np.arange(data.shape[1]),
+             figsize=figsize, **kwargs)
     ax = pyplot.gca()
     ax.set_xticklabels(coords[dims[0]], rotation=45, ha="right")
     ax.set_yticklabels(coords[dims[1]])
