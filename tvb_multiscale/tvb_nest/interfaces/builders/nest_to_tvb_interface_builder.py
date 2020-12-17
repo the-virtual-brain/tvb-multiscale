8cf0c87122ee9e436eb602dc2cf6c0c80d634e7f
dionperd <dionperd@gmail.com>
2020-12-17 17:09:34 +0100

    Renamed tvb, nest interfaces and adjusted to last changes

--- tvb_multiscale/tvb_nest/interfaces/builders/nest_to_tvb_interface_builder.py
+++ tvb_multiscale/tvb_nest/interfaces/builders/nest_to_tvb_interface_builder.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from tvb_multiscale.tvb_nest.interfaces.nest_to_tvb_interfaces import NESTtoTVBinterface
+from tvb_multiscale.tvb_nest.interfaces.nest_to_tvb_interface import NESTtoTVBinterface
 from tvb_multiscale.tvb_nest.nest_models.builders.nest_factory import create_device, connect_device
 from tvb_multiscale.core.spiking_models.builders.factory import build_and_connect_devices
 from tvb_multiscale.core.interfaces.builders.spikeNet_to_tvb_interface_builder import SpikeNetToTVBInterfaceBuilder
