0f533f35a6108643a109b5ead2764a30889c1c3e
dionperd <dionperd@gmail.com>
2020-12-17 17:06:51 +0100

    Refactored tvb_to_spikeNet_device_interface_builder.py

--- tvb_multiscale/core/interfaces/builders/tvb_to_spikeNet_device_interface_builder.py
+++ tvb_multiscale/core/interfaces/builders/tvb_to_spikeNet_device_interface_builder.py
@@ -28,12 +28,18 @@ class TVBtoSpikeNetDeviceInterfaceBuilder(object):
     node_labels = None
     config = CONFIGURED
 
-    def __init__(self, interfaces, spiking_network, spiking_nodes, spiking_nodes_ids,
+    tvb_to_spiking_dt_ratio = config.TVB_TO_SPIKING_DT_RATIO
+    default_min_spiking_dt = config.MIN_SPIKING_DT
+    default_min_delay_ratio = config.MIN_DELAY_RATIO
+    default_min_delay = config.MIN_SPIKING_DT
+
+    default_connection = {}
+
+    def __init__(self, interfaces, spiking_network, spiking_nodes_ids,
                  tvb_nodes_ids, tvb_model, tvb_weights, tvb_delays, node_labels, tvb_dt, exclusive_nodes=False,
                  config=CONFIGURED):
         self.interfaces = interfaces
         self.spiking_network = spiking_network
-        self.spiking_nodes = spiking_nodes
         self.spiking_nodes_ids = spiking_nodes_ids
         self.tvb_nodes_ids = tvb_nodes_ids
         self.tvb_model = tvb_model
@@ -43,6 +49,25 @@ class TVBtoSpikeNetDeviceInterfaceBuilder(object):
         self.tvb_dt = tvb_dt
         self.exclusive_nodes = exclusive_nodes
         self.config = config
+        self._update_default_min_delay()
+        self.default_connection = dict(self.config.DEFAULT_CONNECTION)
+        self.default_connection["delay"] = self.default_min_delay
+
+    @property
+    def spiking_dt(self):
+        return int(np.round(self.tvb_dt / self.tvb_to_spiking_dt_ratio / self.default_min_spiking_dt)) \
+                          * self.default_min_spiking_dt
+
+    @property
+    def min_delay(self):
+        return self.default_min_delay
+
+    def _update_default_min_delay(self):
+        # The Spiking Network min delay should be smaller than half the TVB dt,
+        # and an integer multiple of the spiking simulator dt
+        self.default_min_delay = np.minimum(
+            np.maximum(self.default_min_delay_ratio * self.spiking_dt, self.min_delay),
+            self.tvb_dt / 2)
 
     @abstractmethod
     def build_and_connect_devices(self, devices, nodes, *args, **kwargs):
@@ -68,9 +93,10 @@ class TVBtoSpikeNetDeviceInterfaceBuilder(object):
         # Properties set as functions
         interface_weight_fun = property_to_fun(interface.pop("interface_weights", 1.0))
         interface_weights = np.ones((len(source_tvb_nodes),)).astype("f")
-        weight_fun = property_to_fun(interface.pop("weights", 1.0))
-        delay_fun = property_to_fun(interface.pop("delays", 0.0))
-        receptor_type_fun = property_to_fun(interface.pop("receptor_type", 0))
+        weight_fun = property_to_fun(interface.pop("weights", self.default_connection["weight"]))
+        delay_fun = property_to_fun(interface.pop("delays", self.default_connection["delay"]))
+        receptor_type_fun = property_to_fun(interface.get("receptor_type",
+                                                          self.default_connection["receptor_type"]))
         # Default behavior for any combination of region nodes and populations
         # is to target all of their neurons:
         neurons_inds_fun = interface.pop("neurons_inds", None)
@@ -80,8 +106,9 @@ class TVBtoSpikeNetDeviceInterfaceBuilder(object):
         # Defaults just follow TVB connectivity
         weights = np.array(self.tvb_weights[source_tvb_nodes][:, target_nodes]).astype("O")
         delays = np.array(self.tvb_delays[source_tvb_nodes][:, target_nodes]).astype("O")
-        receptor_type = np.zeros(delays.shape).astype("i")
-        neurons_inds = np.tile([None], delays.shape).astype("O")
+        shape = delays.shape
+        receptor_type = np.tile(self.default_connection["receptor_type"], shape).astype("O")
+        neurons_inds = np.tile([None], shape).astype("O")
         device_names = []
         # Apply now possible functions per source and target region node:
         for src_node in source_tvb_nodes:
@@ -101,25 +128,24 @@ class TVBtoSpikeNetDeviceInterfaceBuilder(object):
         interface["neurons_inds"] = neurons_inds
         interface["nodes"] = [np.where(self.spiking_nodes_ids == trg_node)[0][0] for trg_node in target_nodes]
         # Generate the devices => "proxy TVB nodes":
-        devices = self.build_and_connect_devices([interface], self.spiking_nodes)
+        device_set = self.build_and_connect_devices([interface], self.spiking_network.brain_regions)[0]
         tvb_to_spikeNet_interface = Series()
-        for name, device in devices.items():
-            try:
-                # The TVB state variable index linked to the interface to build
-                tvb_sv_id = self.tvb_model.state_variables.index(name)
-            except:
-                raise_value_error("Interface with %s doesn't correspond to a TVB state variable!")
-            try:
-                interface_builder = self._available_input_device_interfaces[device.model]
-            except:
-                raise_value_error("Interface model %s is not supported yet!" % device.model)
-            interface_index = "%d_%s->%s" % (interface_id, name, str(list(interface["connections"].values())[0]))
-            tvb_to_spikeNet_interface[interface_index] = \
-                interface_builder(self.spiking_network,
-                                  nodes_ids=source_tvb_nodes,
-                                  target_nodes=target_nodes,
-                                  scale=interface_weights,
-                                  dt=self.tvb_dt).from_device_set(device, tvb_sv_id, name)
+        try:
+            # The TVB state variable index linked to the interface to build
+            tvb_sv_id = self.tvb_model.state_variables.index(device_set.name)
+        except:
+            raise_value_error("Interface with %s doesn't correspond to a TVB state variable!")
+        try:
+            interface_builder = self._available_input_device_interfaces[device_set.model]
+        except:
+            raise_value_error("Interface model %s is not supported yet!" % device_set.model)
+        interface_index = "%d_%s->%s" % (interface_id, device_set.name, str(list(interface["connections"].values())[0]))
+        tvb_to_spikeNet_interface[interface_index] = \
+            interface_builder(self.spiking_network,
+                              nodes_ids=source_tvb_nodes,
+                              target_nodes=target_nodes,
+                              scale=interface_weights,
+                              dt=self.tvb_dt).from_device_set(device_set, tvb_sv_id, device_set.name)
         return tvb_to_spikeNet_interface
 
     def build(self):
