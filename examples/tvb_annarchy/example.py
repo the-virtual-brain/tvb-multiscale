03837f2d222eafe8230f3d98c2e13c83fe3e4ad3
dionperd <dionperd@gmail.com>
2020-12-18 20:38:19 +0100

    minor edit in tvb_annarchy example.py

--- examples/tvb_annarchy/example.py
+++ examples/tvb_annarchy/example.py
@@ -119,14 +119,18 @@ def main_example(tvb_sim_model, annarchy_model_builder, tvb_annarchy_builder,
 
     # -------------------------------------------5. Plot results--------------------------------------------------------
     if plot_write:
-        # try:
-        plot_write_results(results, simulator, annarchy_network,
-                               populations=populations, populations_sizes=populations_sizes,
-                               transient=transient, tvb_state_variable_type_label="State Variables",
-                               tvb_state_variables_labels=simulator.model.variables_of_interest,
-                               plot_per_neuron=False, plotter=plotter, config=config)
-        # except Exception as e:
-        #     print("Error in plotting or writing to files!:\n%s" % str(e))
+        try:
+            plot_write_results(results, simulator, annarchy_network,
+                                   populations=populations, populations_sizes=populations_sizes,
+                                   transient=transient, tvb_state_variable_type_label="State Variables",
+                                   tvb_state_variables_labels=simulator.model.variables_of_interest,
+                                   plot_per_neuron=False, plotter=plotter, config=config)
+        except Exception as e:
+            print("Error in plotting or writing to files!:\n%s" % str(e))
+
+    annarchy_network.annarchy_instance.clear()
+
+    del annarchy_network.annarchy_instance
 
     return results, simulator
 
