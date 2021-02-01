67bdc81b4fc30448e8821b361c3e89efede010c0
dionperd <dionperd@gmail.com>
2021-02-01 19:49:13 +0100

    Removed time transformations from core.tvb.transformers

--- tvb_multiscale/core/tvb/transformers.py
+++ tvb_multiscale/core/tvb/transformers.py
@@ -12,9 +12,6 @@ class TVBtoSpikeNetRateTransformer(Scale):
     """TVBtoSpikeNetRateTransformer class that just scales TVB mean field rates to Spiking Network spiking rates,
        including any unit conversions and conversions from mean field to total rates"""
 
-    def compute_time(self):
-        self.output_time = self.dt * np.arange(self.input_time[0], self.input_time[1] + 1)
-
     def compute(self):
         super(TVBtoSpikeNetRateTransformer, self).compute()
         # Assume a TVB (time, voi, proxy) to Spiking Network (proxy, time) reshaping
@@ -27,9 +24,6 @@ class TVBRateToSpikeNetSpikesTransformer(Base):
     """TVBRateToSpikeNetSpikesTransformer class that transforms TVB mean field rates to Spiking Network spikes (times),
        including any unit conversions and conversions from mean field to total rates"""
 
-    def compute_time(self):
-        self.output_time = self.dt * self.input_time
-
     @abstractmethod
     def compute(self, *args, **kwargs):
         """Abstract method for the computation on the input buffer data for the output buffer data to result."""
@@ -40,9 +34,6 @@ class TVBtoSpikeNetCurrentTransformer(Scale):
 
     """TVBRateToSpikeNetSpikesTransformer class that just scales TVB mean field currents to Spiking Network currents,
        including any unit conversions and conversions from mean field to total currents"""
-
-    def compute_time(self):
-        self.output_time = self.dt * np.arange(self.input_time[0], self.input_time[1] + 1)
 
     def compute(self):
         super(TVBtoSpikeNetCurrentTransformer, self).compute()
@@ -55,10 +46,6 @@ class SpikeNetSpikesToTVBRateTransformer(Base):
 
     """SpikeNetSpikesToTVBRateTransformer abstract base class that transforms Spiking Network spikes (times) counts to
        TVB mean field rates, including any unit conversions and conversions from total to mean field rates"""
-
-    def compute_time(self):
-        self.output_time = np.array([np.ceil(self.output_time.min() / self.dt).item(),
-                                     np.ceil(self.output_time.max() / self.dt).item()]).astype("i")
 
     def reshape(self):
         # Assume a Spiking Network (proxy, time) to TVB (time, voi, proxy) reshaping
