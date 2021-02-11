0082c6655f71ba88b4bd565d76036659f15968b1
dionperd <dionperd@gmail.com>
2021-02-11 18:26:06 +0100

    Corrections additions to tvb_nest.interfaces.interfaces

--- tvb_multiscale/tvb_nest/interfaces/interfaces.py
+++ tvb_multiscale/tvb_nest/interfaces/interfaces.py
@@ -2,18 +2,18 @@
 
 from abc import ABCMeta, abstractmethod
 
-import numpy as np
-
-from tvb.basic.neotraits.api import HasTraits, Attr, NArray
+from tvb.basic.neotraits.api import HasTraits, Attr, List
 from tvb.contrib.scripts.utils.data_structures_utils import extract_integer_intervals
 
 from tvb_multiscale.core.tvb.interfaces import \
     TVBtoSpikeNetInterface, SpikeNetToTVBInterface, TVBOutputInterfaces, TVBInputInterfaces
-from tvb_multiscale.core.interfaces.spikeNet.interfaces import \
-    SpikeNetInterfaces, SpikeNetOutputInterface, SpikeNetInputInterface
+from tvb_multiscale.core.interfaces.spikeNet.interfaces import SpikeNetOutputInterfaces, SpikeNetInputInterfaces,\
+    SpikeNetSenderInterface, SpikeNetReceiverInterface, \
+    SpikeNetTransformerSenderInterface, SpikeNetReceiverTransformerInterface
 
 from tvb_multiscale.tvb_nest.interfaces.io import \
-    NESTInputDeviceSetter, NESTEventsFromOutpuDevice, NESTEventsReaderFromRecorderFile
+    NESTInputDeviceSetter, NESTEventsFromOutputDevice
+from tvb_multiscale.tvb_nest.nest_models.network import NESTNetwork
 
 
 class NESTInterface(HasTraits):
@@ -21,48 +21,96 @@ class NESTInterface(HasTraits):
 
     """NESTInterface base class for interfaces sending/receiving data from/to NEST."""
 
-    proxy_gids = NArray(
-        dtype=np.int,
-        label="NEST proxy nodes gids",
-        doc="""NEST proxy nodes gids""",
-        required=True,
-    )
+    spiking_network = Attr(label="NEST Network",
+                           doc="""The instance of NESTNetwork class""",
+                           field_type=NESTNetwork,
+                           required=True)
+
+    @property
+    def nest_network(self):
+        return self.spiking_network
+
+    @property
+    def nest_instance(self):
+        return self.spiking_network.nest_instance
+
+    @property
+    @abstractmethod
+    def proxy_gids(self):
+        pass
+
+    def _get_proxy_gids(self, nest_devices):
+        gids = nest_devices.get("global_id")
+        if isinstance(gids, dict):
+            return gids["global_id"]
 
     @property
     def number_of_proxy_gids(self):
         return self.proxy_gids.shape[0]
 
-    @property
-    @abstractmethod
-    def nest_instance(self):
-        pass
-
     def print_str(self):
         return "\nNEST proxy nodes' gids:\n%s" % extract_integer_intervals(self.proxy_gids, print=True)
 
 
-class NESTOutputInterface(SpikeNetOutputInterface, NESTInterface):
+class NESTOutputInterface(NESTInterface):
 
     """NESTOutputInterface base class for interfaces sending data from NEST."""
 
+    spikeNet_sender_proxy = Attr(label="Spiking network sender proxy",
+                                 doc="""An instance of NESTEventsFromOutputDevice 
+                                        implementing a proxy node sending outputs from the NEST network
+                                        to the co-simulator""",
+                                 field_type=NESTEventsFromOutputDevice,
+                                 required=True)
+
     @property
-    def nest_instance(self):
-        return self.spiking_network.nest_instance
+    def proxy_gids(self):
+        return self._get_proxy_gids(self.spikeNet_sender_proxy.target)
+
+
+class NESTSenderInterface(SpikeNetSenderInterface, NESTOutputInterface):
+
+    """NESTSenderInterface"""
 
     def print_str(self):
-        SpikeNetOutputInterface.print_str(self) + NESTInterface.print_str(self)
+        SpikeNetSenderInterface.print_str(self) + NESTOutputInterface.print_str(self)
 
 
-class NESTInputInterface(SpikeNetInputInterface, NESTInterface):
+class NESTTransformerSenderInterface(SpikeNetTransformerSenderInterface, NESTOutputInterface):
+    """NESTTransformerSenderInterface"""
+
+    def print_str(self):
+        SpikeNetTransformerSenderInterface.print_str(self) + NESTOutputInterface.print_str(self)
+
+
+class NESTInputInterface(NESTInterface):
 
     """NESTInputInterface base class for interfaces receiving data to NEST."""
 
+    spikeNet_receiver_proxy = Attr(label="Spiking network receiver proxy",
+                                   doc="""An instance of ) 
+                                          implementing a proxy node receiving inputs from the co-simulator 
+                                          as an input to the NEST network""",
+                                   field_type=NESTInputDeviceSetter,
+                                   required=True)
+
     @property
-    def nest_instance(self):
-        return self.spiking_network.nest_instance
+    def proxy_gids(self):
+        return self._get_proxy_gids(self.spikeNet_receiver_proxy.source)
+
+
+class NESTReceiverInterface(SpikeNetReceiverInterface, NESTInputInterface):
+    """NESTReceiverInterface"""
 
     def print_str(self):
-        SpikeNetInputInterface.print_str(self) + NESTInterface.print_str(self)
+        SpikeNetReceiverInterface.print_str(self) + NESTInputInterface.print_str(self)
+
+
+class NESTReceiverTransformerInterface(SpikeNetReceiverTransformerInterface, NESTInputInterface):
+    """NESTReceiverTransformerInterface"""
+
+    def print_str(self):
+        SpikeNetReceiverTransformerInterface.print_str(self) + NESTInputInterface.print_str(self)
 
 
 class TVBtoNESTInterface(TVBtoSpikeNetInterface, NESTOutputInterface):
@@ -70,13 +118,6 @@ class TVBtoNESTInterface(TVBtoSpikeNetInterface, NESTOutputInterface):
     """TVBtoNESTInterface class to get data from TVB, transform them,
        and finally set them to NEST, all processes taking place in shared memmory.
     """
-
-    communicator = Attr(
-        label="Communicator directly to NEST",
-        field_type=NESTInputDeviceSetter,
-        doc="""A NESTInputDeviceSetter Communicator class instance to send data to NEST.""",
-        required=True
-    )
 
     def print_str(self):
         TVBtoSpikeNetInterface.print_str(self) + NESTOutputInterface.print_str(self)
@@ -88,53 +129,51 @@ class NESTtoTVBInterface(SpikeNetToTVBInterface, NESTInputInterface):
        and finally set them to TVB, all processes taking place in shared memmory.
     """
 
-    communicator = Attr(
-        label="Communicator directly from NEST",
-        field_type=(NESTEventsFromOutpuDevice, NESTEventsReaderFromRecorderFile),
-        doc="""A NESTEventsFromOutpuDevice or NESTEventsReaderFromRecorderFile Communicator class instance 
-               to receive events' data from NEST.""",
-        required=True
-    )
-
     def print_str(self):
         SpikeNetToTVBInterface.print_str(self) + NESTInputInterface.print_str(self)
 
 
 class NESTInterfaces(HasTraits):
-    __metaclass__ = ABCMeta
 
     """NESTInterfaces class holding a list of NESTInterface instances"""
 
+    interfaces = List(of=NESTInterface)
+
     @property
     def proxy_gids(self):
-        return np.sort(self._loop_get_from_interfaces("proxy_gids"))
+        return self._loop_get_from_interfaces("proxy_gids")
 
     @property
     def number_of_proxy_gids(self):
         return self.proxy_gids.shape[0]
 
     @property
-    @abstractmethod
+    def nest_network(self):
+        if len(self.interfaces):
+            return self.interfaces[0].spiking_network
+        else:
+            return None
+
+    @property
     def nest_instance(self):
-        pass
+        if len(self.interfaces):
+            return self.interfaces[0].nest_instance
+        else:
+            return None
 
 
-class NESTOutputInterfaces(SpikeNetInterfaces, NESTInterfaces):
+class NESTOutputInterfaces(SpikeNetOutputInterfaces, NESTInterfaces):
 
     """NESTOutputInterfaces holding a list of NESTOutputInterface instances"""
 
-    @property
-    def nest_instance(self):
-        return self.spiking_network.nest_instance
+    pass
 
 
-class NESTInputInterfaces(SpikeNetInterfaces, NESTInterfaces):
+class NESTInputInterfaces(SpikeNetInputInterfaces, NESTInterfaces):
 
     """NESTInputInterfaces holding a list of NESTInputInterface instances"""
 
-    @property
-    def nest_instance(self):
-        return self.spiking_network.nest_instance
+    pass
 
 
 class TVBtoNESTInterfaces(TVBOutputInterfaces, NESTInputInterfaces):
