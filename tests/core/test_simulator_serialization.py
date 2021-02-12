edd7818dba9f8167606113d27678c4a25f51c2a7
dionperd <dionperd@gmail.com>
2021-02-12 20:36:55 +0100

    Upgraded cosimulator_serialization.py - test adjustment

--- tests/core/test_simulator_serialization.py
+++ tests/core/test_simulator_serialization.py
@@ -13,21 +13,21 @@ from tvb.simulator.models.reduced_wong_wang_exc_io import ReducedWongWangExcIO
 from tvb.simulator.models.reduced_wong_wang_exc_io_inh_i import ReducedWongWangExcIOInhI
 
 from tvb_multiscale.core.config import CONFIGURED
-from tvb_multiscale.core.tvb.simulator_builder import SimulatorBuilder
-from tvb_multiscale.core.tvb.simulator_serialization import \
-    serialize_tvb_simulator, dump_serial_tvb_simulator, load_serial_tvb_simulator
+from tvb_multiscale.core.tvb.cosimulator_builder import CoSimulatorBuilder
+from tvb_multiscale.core.tvb.cosimulator_serialization import \
+    serialize_tvb_cosimulator, dump_serial_tvb_cosimulator, load_serial_tvb_cosimulator
 
 
 def test_simulator_serialization(test_models=[WilsonCowan, ReducedWongWangExcIO, ReducedWongWangExcIOInhI]):
     for test_model in test_models:
-        simulator_builder = SimulatorBuilder()
+        simulator_builder = CoSimulatorBuilder()
         simulator_builder.connectivity = CONFIGURED.DEFAULT_CONNECTIVITY_ZIP
         simulator_builder.model = test_model
         simulator = simulator_builder.build()
-        serial_sim = serialize_tvb_simulator(simulator)
+        serial_sim = serialize_tvb_cosimulator(simulator)
         filepath = os.path.join(CONFIGURED.out.FOLDER_RES, serial_sim["model"]+".dil")
-        dump_serial_tvb_simulator(filepath)
-        serial_sim2 = load_serial_tvb_simulator()
+        dump_serial_tvb_cosimulator(filepath)
+        serial_sim2 = load_serial_tvb_cosimulator()
         for key, val in serial_sim.items():
             assert np.all(serial_sim2[key] == val)
 
