a78dcadfa29b235018cff5d71d07603aad697f01
dionperd <dionperd@gmail.com>
2021-03-01 16:15:40 +0100

    Corrected annarchy_factory for TimedArray + spiking populations' devices creation

--- tvb_multiscale/tvb_annarchy/annarchy_models/builders/annarchy_factory.py
+++ tvb_multiscale/tvb_annarchy/annarchy_models/builders/annarchy_factory.py
@@ -257,6 +257,9 @@ def create_input_device(annarchy_device, import_path, params={}, config=CONFIGUR
     annarchy_device._population = create_population(annarchy_device.model, annarchy_device.annarchy_instance,
                                                     params=params, import_path=import_path, config=config)
     annarchy_device._population.name = annarchy_device.label
+    if isinstance(annarchy_device, tuple(ANNarchyTimedArraySpikeInputDeviceDict.values())):
+        proj = annarchy_device.annarchy_instance.Projection(annarchy_device.device, annarchy_device._population, "exc")
+        proj = proj.connect_all_to_all(1.0)
     return annarchy_device
 
 
@@ -339,10 +342,13 @@ def connect_input_device(annarchy_device, population, neurons_inds_fun=None,
         synapse = None
     if synapse is not None:
         syn_spec["synapse"] = assert_model(synapse, annarchy_device.annarchy_instance, import_path)
+    if isinstance(annarchy_device, tuple(ANNarchyTimedArraySpikeInputDeviceDict.values())):
+        # We need to take into consideration the delay from the TimedArray to the spiking generating population:
+        delay = np.maximum(delay - annarchy_device.annarchy_instance.dt(), 0.0)
     proj = connect_two_populations(annarchy_device, population,
-                                   weight, delay, receptor_type, syn_spec, conn_spec,
+                                   weight, delay, receptor_type, syn_spec, connection_args,
                                    source_view_fun=None, target_view_fun=neurons_inds_fun,
-                                   annarchy_instance=annarchy_device.annarchy_instance, **connection_args)
+                                   annarchy_instance=annarchy_device.annarchy_instance)
     # Add this projection to the source device's and target population's inventories:
     annarchy_device.projections_pre.append(proj)
     population.projections_post.append(proj)
