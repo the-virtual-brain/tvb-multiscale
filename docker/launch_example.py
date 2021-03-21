a4351bc03e95bee000a9c1dc5b1e66f2cf16aa06
dionperd <dionperd@gmail.com>
2021-03-21 16:09:29 +0100

    Setting default_example to docker launch_example.py

--- docker/launch_example.py
+++ docker/launch_example.py
@@ -11,7 +11,7 @@ import numpy as np
 from tvb_multiscale.tvb_nest.config import Config
 from tvb.datatypes.connectivity import Connectivity
 
-from examples.tvb_nest.models.wilson_cowan import wilson_cowan_example
+from examples.tvb_nest.example import default_example
 
 
 def launch_example(write_files=True, **kwargs):
@@ -21,15 +21,9 @@ def launch_example(write_files=True, **kwargs):
     config.figures.SHOW_FLAG = False
     config.figures.MATPLOTLIB_BACKEND = "Agg"
 
-    # Select the regions for the fine scale modeling with NEST spiking networks
-    nest_nodes_ids = []  # the indices of fine scale regions modeled with NEST
-    # In this example, we model parahippocampal cortices (left and right) with NEST
     connectivity = Connectivity.from_file(config.DEFAULT_CONNECTIVITY_ZIP)
-    for id in range(connectivity.region_labels.shape[0]):
-        if connectivity.region_labels[id].find("hippo") > 0:
-            nest_nodes_ids.append(id)
 
-    results, simulator = wilson_cowan_example(connectivity=connectivity, config=config, **kwargs)
+    results, simulator = default_example(connectivity=connectivity, config=config, **kwargs)
 
     if write_files:
         np.save(os.path.join(config.out.FOLDER_RES, "connectivity_weights.npy"), simulator.connectivity.weights)
