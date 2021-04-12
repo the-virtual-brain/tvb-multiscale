600b968341fb6bb0193fc0ec3fbd9e96bf52b6e9
dionperd <dionperd@gmail.com>
2021-04-12 19:16:27 +0200

    Added g_exc monitor for annarchy_models/models/basal_ganglia_izhikevich.py model builder

--- tvb_multiscale/tvb_annarchy/annarchy_models/builders/models/basal_ganglia_izhikevich.py
+++ tvb_multiscale/tvb_annarchy/annarchy_models/builders/models/basal_ganglia_izhikevich.py
@@ -175,7 +175,7 @@ class BasalGangliaIzhikevichBuilder(ANNarchyModelBuilder):
         # TODO: use baladron neurons
         params = deepcopy(self.config.ANNARCHY_OUTPUT_DEVICES_PARAMS_DEF["Monitor"])
         params.update({"period": 1.0, 'record_from': ["v", "u", "I", "I_syn", "I_syn_ex", "I_syn_in",
-                                                      "g_ampa", "g_gaba", "g_base"]})
+                                                      "g_ampa", "g_gaba", "g_base", "g_exc"]})
         for pop in self.populations:
             connections = OrderedDict({})
             #               label    <- target population
