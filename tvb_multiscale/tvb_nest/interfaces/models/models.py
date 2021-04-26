b65ad96df1579d933abed283aeab2d6c42306f06
dionperd <dionperd@gmail.com>
2021-04-26 19:43:02 +0300

    Adjusted Linear tvb_nest interface model and the TVB cosim models used

--- tvb_multiscale/tvb_nest/interfaces/models/models.py
+++ tvb_multiscale/tvb_nest/interfaces/models/models.py
@@ -3,17 +3,17 @@
 from tvb_multiscale.tvb_nest.config import CONFIGURED, initialize_logger
 from tvb_multiscale.tvb_nest.interfaces.base import TVBNESTInterface
 
-from tvb.simulator.models.reduced_wong_wang_exc_io import ReducedWongWangExcIO
-from tvb.simulator.models.reduced_wong_wang_exc_io_inh_i import ReducedWongWangExcIOInhI
-from tvb.simulator.models.wilson_cowan_constraint import WilsonCowan as WilsonCowanSimModel
-from tvb.simulator.models.generic_2d_oscillator_multiscale import Generic2dOscillator as Generic2dOscillatorSimModel
+from tvb_multiscale.core.tvb.cosimulator.models.linear import Linear
+from tvb_multiscale.core.tvb.cosimulator.models.reduced_wong_wang_exc_io import ReducedWongWangExcIO
+from tvb_multiscale.core.tvb.cosimulator.models.reduced_wong_wang_exc_io_inh_i import ReducedWongWangExcIOInhI
+from tvb_multiscale.core.tvb.cosimulator.models.wilson_cowan_constraint import WilsonCowan as WilsonCowanSimModel
 
 
 LOG = initialize_logger(__name__)
 
 
 class Linear(TVBNESTInterface):
-    tvb_model = ReducedWongWangExcIO()
+    tvb_model = Linear()
 
     def __init__(self, config=CONFIGURED):
         super(Linear, self).__init__(config)
@@ -33,14 +33,6 @@ class RedWWexcIOinhI(TVBNESTInterface):
 
     def __init__(self, config=CONFIGURED):
         super(RedWWexcIOinhI, self).__init__(config)
-        LOG.info("%s created!" % self.__class__)
-
-
-class Generic2dOscillator(TVBNESTInterface):
-    tvb_model = Generic2dOscillatorSimModel()
-
-    def __init__(self, config=CONFIGURED):
-        super(Generic2dOscillator, self).__init__(config)
         LOG.info("%s created!" % self.__class__)
 
 
