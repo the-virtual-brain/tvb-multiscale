71cd9646b7f4472393959ee892a175fa365c9432
dionperd <dionperd@gmail.com>
2021-04-26 19:33:43 +0300

    Substituted Linear TVB cosim model with linear_with_stimulus.py

--- tvb_multiscale/core/tvb/cosimulator/models/linear.py
+++ tvb_multiscale/core/tvb/cosimulator/models/linear.py
@@ -32,39 +32,86 @@ Generic linear model.
 """
 
 import numpy
-from tvb.simulator.models.base import Model
 from tvb.basic.neotraits.api import NArray, Final, List, Range
+from tvb.simulator.models.linear import Linear as TVBLinear
 
 
-class Linear(Model):
-    gamma = NArray(
-        label=r":math:`\gamma`",
-        default=numpy.array([-10.0]),
-        domain=Range(lo=-100.0, hi=0.0, step=1.0),
-        doc="The damping coefficient specifies how quickly the node's activity relaxes, must be larger"
-            " than the node's in-degree in order to remain stable.")
+class Linear(TVBLinear):
+    I_o = NArray(
+        label=r":math:`I_o`",
+        default=numpy.array([0.0]),
+        domain=Range(lo=-100.0, hi=100.0, step=1.0),
+        doc="External stimulus")
+
+    G = NArray(
+        label=r":math:`G`",
+        default=numpy.array([0.0]),
+        domain=Range(lo=-0.0, hi=100.0, step=1.0),
+        doc="Global coupling scaling")
+
+    tau = NArray(
+        label=r":math:`\tau`",
+        default=numpy.array([1.0]),
+        domain=Range(lo=-0.1, hi=100.0, step=0.1),
+        doc="Time constant")
+
+    tau_rin = NArray(
+        label=r":math:`\tau_rin_e`",
+        default=numpy.array([10., ]),
+        domain=Range(lo=1., hi=100., step=1.0),
+        doc="""[ms]. Excitatory population instant spiking rate time constant.""")
+
+    # Used for phase-plane axis ranges and to bound random initial() conditions.
+    state_variable_boundaries = Final(
+        default={"R": numpy.array([0.0, None]),
+                 "Rin": numpy.array([0.0, None])},
+        label="State Variable boundaries [lo, hi]",
+        doc="""The values for each state-variable should be set to encompass
+                the boundaries of the dynamic range of that state-variable. 
+                Set None for one-sided boundaries""")
 
     state_variable_range = Final(
         label="State Variable ranges [lo, hi]",
-        default={"x": numpy.array([-1, 1])},
+        default={"R": numpy.array([0, 100]),
+                 "Rin": numpy.array([0, 100])},
         doc="Range used for state variable initialization and visualization.")
 
     variables_of_interest = List(
         of=str,
         label="Variables watched by Monitors",
-        choices=("x",),
-        default=("x",), )
+        choices=("R", 'Rin'),
+        default=("R", 'Rin'), )
 
-    state_variables = ('x',)
-    _nvar = 1
+    state_variables = ('R', 'Rin')
+    integration_variables = ('R',)
+    _nvar = 2
     cvar = numpy.array([0], dtype=numpy.int32)
+
+    def update_derived_parameters(self):
+        """
+        When needed, this should be a method for calculating parameters that are
+        calculated based on paramaters directly set by the caller. For example,
+        see, ReducedSetFitzHughNagumo. When not needed, this pass simplifies
+        code that updates an arbitrary models parameters -- ie, this can be
+        safely called on any model, whether it's used or not.
+        """
+        if hasattr(self, "Rin"):
+            setattr(self, "_Rin", getattr(self, "Rin") > 0)
+        else:
+            setattr(self, "Rin", numpy.array([0.0, ]))
+            setattr(self, "_Rin", numpy.array([False, ]))
+
+    def update_non_state_variables_after_integration(self, state_variables):
+        # Reset to 0 the Rin for nodes not updated by Spiking Network
+        state_variables[1] = numpy.where(self._Rin, state_variables[1], 0.0)
+        return state_variables
 
     def dfun(self, state, coupling, local_coupling=0.0):
         """
         .. math::
-            x = a{\gamma} + b
+            dR/dt = (-R + G * coupling) / {\tau} + I_o
         """
-        x, = state
-        c, = coupling
-        dx = self.gamma * x + c + local_coupling * x
-        return numpy.array([dx])
+        dR = numpy.where(self._Rin,
+                         (- state[0] + state[1]) / self.tau_rin,
+                         (-state[0] + self.G * coupling[0] + local_coupling * state[0] ) / self.tau + self.I_o)
+        return numpy.array([dR, 0.0*dR])
