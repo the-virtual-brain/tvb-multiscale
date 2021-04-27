c1eab247214ce2735d77fe7e1b58c32ec2e1df4d
dionperd <dionperd@gmail.com>
2021-04-27 21:48:04 +0300

    Corrected ANNARCHY_INPUT_DEVICES_PARAMS_DEF in tvb_annarchy config.py

--- tvb_multiscale/tvb_annarchy/config.py
+++ tvb_multiscale/tvb_annarchy/config.py
@@ -45,21 +45,16 @@ class Config(ConfigBase):
                                           "spike_multimeter": {"record_from": "spike", "period": 1.0},
                                           "Monitor": {"record_from": ["v", 'g_exc', 'g_inh'], "period": 1.0}}
 
-    ANNARCHY_INPUT_DEVICES_PARAMS_DEF = {"SpikeSourceArray": {"spike_times": [[1.0]]},
+    ANNARCHY_INPUT_DEVICES_PARAMS_DEF = {"SpikeSourceArray": {"spike_times": [[0.1]]},
                                          "PoissonPopulation": {"rates": 0.0},
                                          "Poisson_neuron": {"rates": 0.0},
-                                         "HomogeneousCorrelatedSpikeTrains": {"rates": 1.0, "corr": 0.0, "tau": 1.0},
-                                         "TimedArray": {"rates": np.array([[0.0]]), "schedule": [0.0], "period": -1.0,
+                                         "HomogeneousCorrelatedSpikeTrains": {"rates": 0.001, "corr": 0.0, "tau": 1.0,
+                                                                              "schedule": None, "period": -1.0,
+                                                                              "refractory": None},
+                                         "TimedArray": {"rates": np.array([[0.0]]), "schedule": None, "period": -1.0,
                                                         "proxy": True, "proxy_target": "exc"},
                                          "TimedPoissonPopulation": {"rates": np.array([[0.0]]),
-                                                                    "schedule": [0.0], "period": -1.0},
-                                         # "TimedHomogeneousCorrelatedSpikeTrains":
-                                         #     {"rates": np.array([[1.0]]), "corr": 0.0, "tau": 1.0,
-                                         #      "schedule": [0.0], "period": -1.0}
-                                         # "CurrentInjector": {"amplitude": 0.0},
-                                         # "DCCurrentInjector": {"amplitude": 0.0},
-                                         # "ACCurrentInjector": {"frequency": 0.0, "amplitude": 1.0,
-                                         #                       "phase": 0.0, "offset": 0.0},
+                                                                    "schedule": None, "period": -1.0},
                                          }
 
     def __init__(self, output_base=None, separate_by_run=False, initialize_logger=True):
