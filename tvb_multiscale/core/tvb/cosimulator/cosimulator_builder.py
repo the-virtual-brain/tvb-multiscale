8d525b2ec557c85a7ac4a53bac1ff4d2bd78b835
dionperd <dionperd@gmail.com>
2021-06-10 17:54:35 +0200

    Changed cosimulator_builder.py defaults for ceiling (1.0 -> No), symmetric connectome forcing (yes -> no)

--- tvb_multiscale/core/tvb/cosimulator/cosimulator_builder.py
+++ tvb_multiscale/core/tvb/cosimulator/cosimulator_builder.py
@@ -97,9 +97,9 @@ class CoSimulatorBuilder(HasTraits):
 
     ceil_connectivity = Float(
         label="Connectivity ceiling",
-        default=1.0,
+        default=0.0,
         required=False,
-        doc="""Value to ceil connectivity weights to. Default = 1.0""")
+        doc="""Value to ceil connectivity weights to. Default = 0.0, which will not ceil connectivity""")
 
     symmetric_connectome = Attr(label="Symmetric connectome flag",
                                 doc="""Boolean flag that if True 
@@ -114,7 +114,7 @@ class CoSimulatorBuilder(HasTraits):
                                           self-connections of brain region nodes are forced to a zero weight.
                                           Default = True""",
                                    field_type=bool,
-                                   default=True,
+                                   default=False,
                                    required=True)
 
     delays_flag = Attr(label="Delays' flag",
