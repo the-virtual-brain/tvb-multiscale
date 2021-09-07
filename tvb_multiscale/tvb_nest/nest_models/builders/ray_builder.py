5fda064887481ec63c1b99a9103f9e87b8a87691
dionperd <dionperd@gmail.com>
2021-09-07 15:24:02 +0200

    Refactoring for create_ray_nest_network_builder_type

--- tvb_multiscale/tvb_nest/nest_models/builders/ray_builder.py
+++ tvb_multiscale/tvb_nest/nest_models/builders/ray_builder.py
@@ -1,22 +1,31 @@
 # -*- coding: utf-8 -*-
 
+from types import MethodType
+
 from tvb_multiscale.core.ray.client import create_ray_client
+from tvb_multiscale.core.spiking_models.builders.ray_builder import \
+    RunLock, RaySpikingNetworkBuilder, create_ray_spiking_network_builder_type
 from tvb_multiscale.tvb_nest.nest_models.network import NESTNetwork
+from tvb_multiscale.tvb_nest.nest_models.builders.base import NESTNetworkBuilder
 
 
-def create_ray_spiking_network_builder(spiking_network_builder):
+class RayNESTNetworkBuilder(NESTNetworkBuilder, RaySpikingNetworkBuilder):
 
-    class RaySpikingNetworkBuilder(spiking_network_builder):
+    def build_spiking_network(self):
+        """A method to build the final NESTNetwork class based on the already created constituents."""
+        ray_nest_network = \
+            create_ray_client(NESTNetwork, non_blocking_methods=["Run", "Simulate"],
+                              nest_instance=self.nest_instance,
+                              brain_regions=self._spiking_brain,
+                              output_devices=self._output_devices,
+                              input_devices=self._input_devices,
+                              config=self.config)
+        setattr(ray_nest_network, "RunLock", MethodType(RunLock, ray_nest_network))
+        return ray_nest_network
 
-        def build_spiking_network(self):
-            """A method to build the final NESTNetwork class based on the already created constituents."""
-            return create_ray_client(NESTNetwork, non_blocking_methods=["Run", "Simulate"],
-                                     nest_instance=self.nest_instance,
-                                     brain_regions=self._spiking_brain,
-                                     output_devices=self._output_devices,
-                                     input_devices=self._input_devices,
-                                     config=self.config)
 
-    RaySpikingNetworkBuilder.__name__ = "Ray%s" % spiking_network_builder.__name__
+def create_ray_nest_network_builder_type(nest_network_builder=NESTNetworkBuilder,
+                                         ray_builder=RayNESTNetworkBuilder):
 
-    return RaySpikingNetworkBuilder
+    return create_ray_spiking_network_builder_type(spiking_network_builder=nest_network_builder,
+                                                   ray_builder=ray_builder)
