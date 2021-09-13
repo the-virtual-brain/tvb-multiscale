7b8c0ad9f46725eb092a5df2f30232bac355969e
dionperd <dionperd@gmail.com>
2021-09-13 21:52:52 +0300

    Modification of random number generator kernel status inputs for NEST 3.0

--- tvb_multiscale/tvb_nest/config.py
+++ tvb_multiscale/tvb_nest/config.py
@@ -23,7 +23,7 @@ class Config(ConfigBase):
     MYMODULES_DIR = MYMODULES_DIR
     MYMODULES_BLD_DIR = MYMODULES_BLD_DIR
 
-    MASTER_SEED = 0
+    MASTER_SEED = 1
 
     # NEST properties:
     # M_ALL=0,  display all messages
@@ -85,13 +85,8 @@ class Config(ConfigBase):
 
     @property
     def DEFAULT_NEST_KERNEL_CONFIG(self):
-        # TODO: Find how to compute this:
-        TOTAL_NUM_VIRTUAL_PROCS = self.TOTAL_NUM_VIRTUAL_PROCS
         return {"data_path": self.RECORDINGS_DIR, "overwrite_files": True, "print_time": True,
-                'grng_seed': self.MASTER_SEED + TOTAL_NUM_VIRTUAL_PROCS,
-                'rng_seeds': range(self.MASTER_SEED + 1 + TOTAL_NUM_VIRTUAL_PROCS,
-                                   self.MASTER_SEED + 1 + (2 * TOTAL_NUM_VIRTUAL_PROCS)),
-                "local_num_threads": self.DEFAULT_LOCAL_NUM_THREADS}
+                'rng_seed': self.MASTER_SEED, "local_num_threads": self.DEFAULT_LOCAL_NUM_THREADS}
 
     @property
     def DEFAULT_CONNECTION(self):
