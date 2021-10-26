44eafab22127b3e65b4b2cefec8f81dd82148ef0
dionperd <dionperd@gmail.com>
2021-10-26 17:24:25 +0200

    Added commented example for ICA function in utils.py of cerebellum notebook example

--- examples/tvb_nest/notebooks/cerebellum/utils.py
+++ examples/tvb_nest/notebooks/cerebellum/utils.py
@@ -127,3 +127,31 @@ def compute_plot_ica(data, time, variable="BOLD", n_components=10, plotter=None)
         plt.colorbar()
         plt.tight_layout()
     return ica.components_, ics_ts, ica  # (ICA components, ICA components time series, ICA class instance)
+
+# Example about how ICA works:
+# from sklearn.decomposition import FastICA
+# np.random.seed(0)
+# n_samples = 2000
+# time = np.linspace(0, 8, n_samples)
+# s1 = np.sin(2 * time)
+# s2 = np.sign(np.sin(3 * time))
+# s3 = signal.sawtooth(2 * np.pi * time)
+# S = np.c_[s1, s2, s3]
+# S += 0.2 * np.random.normal(size=S.shape)
+# S /= S.std(axis=0)
+# A = np.array([[1, 1, 1], [0.5, 2, 1.0], [1.5, 1.0, 2.0]])
+# X = np.dot(S, A.T)
+# ica = FastICA(n_components=3)
+# S_ = ica.fit_transform(X)
+# fig = plt.figure()
+# models = [X, S, S_]
+# names = ['mixtures', 'real sources', 'predicted sources']
+# colors = ['red', 'blue', 'orange']
+# for i, (name, model) in enumerate(zip(names, models)):
+#     plt.subplot(4, 1, i+1)
+#     plt.title(name)
+#     for sig, color in zip (model.T, colors):
+#         plt.plot(sig, color=color)
+
+# fig.tight_layout()
+# plt.show()
