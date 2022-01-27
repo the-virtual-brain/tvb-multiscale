f8c6e70f89637727fba12ad1a71c319371210cc6
dionperd <dionperd@gmail.com>
2022-01-27 12:47:14 +0100

    Added example_script_BIH.sh

---
+++ docker/hpc/example_script_BIH.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#SBATCH --job-name=job1
+
+#SBATCH --ntasks 32
+
+#SBATCH --nodes=1
+
+#SBATCH --time 7-00:00:00
+
+#SBATCH --mem-per-cpu=3G
+
+#SBATCH --partition=medium
+
+singularity bash --bind tvb-root/:/home/docker/packages/tvb-root,tvb-multiscale/:/home/docker/packages/tvb-multiscale multi.sif
+
+home/docker/env/neurosci/bin/python /home/docker/packages/tvb-multiscale/examples/tvb_nest/example.py
+
+export DOCKER_HOME=/home/docker
+export DOCKER_TVB_ROOT=$DOCKER_HOME/packages/tvb-root
+export DOCKER_TVB_MULTISCALE=$DOCKER_HOME/packages/tvb-multiscale
+export DOCKER_WORKDIR=$DOCKER_TVB_MULTISCALE/tvb_multiscale/examples/tvb_nest
+export DOCKER_PYTHON=/home/docker/env/neurosci/bin/python
+export DOCKER_RUN_FILE=/home/docker/packages/tvb-multiscale/tvb_multiscale/examples/tvb_nest/example.py #here we need to put our python script
+export CMD=$DOCKER_PYTHON' '$DOCKER_RUN_FILE
+
+export NODES=mc  # or gpu, nothing for BIH cluster
+export TIME=24:00:00
+export OUTPUT_FILE='$SCRATCH/slurm/output.out'
+export MAIL=...@gmail.con
+export SRUN='srun -C '$NODES' -t '$TIME' -o '$OUTPUT_FILE' --mail-type=ALL --mail-user='$MAIL
+
+
+# Sarus
+## In PizDaint, we assume that these lines have been already ran:
+#module load daint-mc # or daint-gpu
+#module load sarus
+export DOCKER_IMAGE='thevirtualbrain/tvb-nest:ongoing_dp_work'
+
+export INTERACTIVE_CMD='sarus --debug run -t --mpi --workdir='$DOCKER_WORKDIR'
+                              --mount=type=bind,source=${TVB_MULTISCALE},destination=$'DOCKER_TVB_MULTISCALE'
+                              --mount=type=bind,source=${TVB_ROOT},destination=$'DOCKER_TVB_ROOT'
+                         '$DOCKER_IMAGE' bash'
+
+export SRUN_INTERACTIVE_CMD=$SRUN' --pty -C '$INTERACTIVE_CMD
+
+export COMMAND='sarus --debug run --mpi --workdir='$DOCKER_WORKDIR'
+                      --mount=type=bind,source=${TVB_MULTISCALE},destination=$'DOCKER_TVB_MULTISCALE'
+                      --mount=type=bind,source=${TVB_ROOT},destination=$'DOCKER_TVB_ROOT'
+                '$DOCKER_IMAGE' '$CMD
+
+export SRUN_COMMAND=$SRUN' '$COMMAND
+
+## Singularity
+### In PizDaint, we assume that these lines have been already ran:
+##module load daint-mc # or daint-gpu
+##module load singularity/3.5.3-daint
+## singularity pull docker://${DOCKER_IMAGE}
+#export DOCKER_IMAGE_RUN='tvb-nest_dp-ongoing-work.sif'
+#
+## Run from within the path where the .sif file is:
+#
+#export INTERACTIVE_CMD='singularity shell
+#                        --bind $TVB_MULTISCALE':$DOCKER_TVB_MULTISCALE'
+#                        --bind $TVB_ROOT:'$DOCKER_TVB_ROOT'
+#                        '$DOCKER_IMAGE_RUN
+#
+#export SRUN_INTERACTIVE_CMD=$SRUN' -pty -C '$INTERACTIVE_CMD
+#
+#export COMMAND='singularity exec
+#                --bind $TVB_MULTISCALE:'$DOCKER_TVB_MULTISCALE'
+#                --bind $TVB_ROOT:'$DOCKER_TVB_ROOT'
+#                '$DOCKER_IMAGE' '$CMD
+#
+#export SRUN_COMMAND=$SRUN' '$COMMAND
+
+
+
