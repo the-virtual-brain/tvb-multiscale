d413dc842aa9ef3e0d060ef96b66cbf18e2dd2db
Valeriy Bragin <vbragin19@gmail.com>
2022-04-03 18:44:58 +0000

    tvb-netpyne initial commit

---
+++ tvb_multiscale/tvb_netpyne/interfaces/builders.py
@@ -0,0 +1,174 @@
+from logging import Logger
+from enum import Enum
+
+import numpy as np
+
+from tvb.basic.neotraits.api import Attr
+
+from tvb_multiscale.core.interfaces.tvb.builders import TVBSpikeNetInterfaceBuilder
+from tvb_multiscale.core.interfaces.tvb.interfaces import TVBtoSpikeNetModels, SpikeNetToTVBModels
+from tvb_multiscale.core.interfaces.spikeNet.builders import SpikeNetProxyNodesBuilder, SpikeNetInterfaceBuilder, \
+    SpikeNetRemoteInterfaceBuilder
+from tvb_multiscale.core.spiking_models.builders.factory import build_and_connect_devices
+
+from tvb_multiscale.tvb_netpyne.config import Config, CONFIGURED, initialize_logger
+from tvb_multiscale.tvb_netpyne.interfaces.interfaces import \
+    NetpyneOutputInterface, NetpyneInputInterface, \
+    TVBtoNetpyneInterface, NetpyneToTVBInterface
+from tvb_multiscale.tvb_netpyne.interfaces.io import \
+    NetpyneSpikeRecorderSet, NetpyneSpikeRecorderTotalSet, \
+    NetpynePoissonGeneratorSet
+from tvb_multiscale.tvb_netpyne.netpyne_models.network import NetpyneNetwork
+from tvb_multiscale.tvb_netpyne.netpyne_models.builders.netpyne_factory import create_device, connect_device
+
+
+TVBtoNetpyneModels = TVBtoSpikeNetModels
+NetpyneToTVBModels = SpikeNetToTVBModels
+
+
+class NetpyneInputProxyModels(Enum):
+    RATE = NetpynePoissonGeneratorSet
+    SPIKES = None
+
+
+class NetpyneOutputProxyModels(Enum):
+    SPIKES = NetpyneSpikeRecorderSet
+    SPIKES_MEAN = NetpyneSpikeRecorderTotalSet
+
+
+class DefaultTVBtoNetpyneModels(Enum):
+    RATE = NetpyneInputProxyModels.RATE.name
+    SPIKES = None
+
+
+class DefaultNetpyneToTVBModels(Enum):
+    SPIKES = NetpyneOutputProxyModels.SPIKES_MEAN.name
+
+
+class NetpyneProxyNodesBuilder(SpikeNetProxyNodesBuilder):
+
+    """NetpyneProxyNodesBuilder class"""
+
+    spiking_network = Attr(label="NetPyNE Network",
+                           doc="""The instance of NetpyneNetwork class""",
+                           field_type=NetpyneNetwork,
+                           required=True)
+
+
+    @property
+    def netpyne_network(self):
+        return self.spiking_network
+
+    @property
+    def netpyne_instance(self):
+        return self.spiking_network.netpyne_instance
+
+    @property
+    def spikeNet_min_delay(self):
+        return self.netpyne_instance.minDelay
+
+    @property
+    def netpyne_min_delay(self):
+        return self.netpyne_instance.minDelay
+
+    def _build_and_connect_devices(self, interface, **kwargs):
+        return build_and_connect_devices(interface, create_device, connect_device,
+                                         self.spiking_network.brain_regions,
+                                         self.config, netpyne_instance=self.netpyne_instance, **kwargs)
+
+    def _build_and_connect_output_devices(self, interface, **kwargs):
+        return self._build_and_connect_devices(interface, **kwargs)
+
+    def _build_and_connect_input_devices(self, interface, **kwargs):
+        interface["weights"] = interface["weights"] * self.netpyne_synaptic_weight_scale
+        popSizes = {"E": self.N_E, "I": self.N_I} # TODO: de-hardcode (works for RedWongWang, but might not work for other models)
+        return self._build_and_connect_devices(interface, popSizes = popSizes, lamda = self.lamda, **kwargs)
+
+    def _default_receptor_type(self, source_node, target_node):
+        return 0
+
+    @property
+    def _default_min_delay(self):
+        return self.netpyne_min_delay
+
+
+class NetpyneInterfaceBuilder(NetpyneProxyNodesBuilder, SpikeNetInterfaceBuilder):
+    """NetpyneInterfaceBuilder class"""
+
+    _tvb_to_spikeNet_models = TVBtoNetpyneModels
+    _spikeNet_to_tvb_models = NetpyneToTVBModels
+
+    _default_tvb_to_spikeNet_models = DefaultTVBtoNetpyneModels
+    _default_spikeNet_to_tvb_models = DefaultNetpyneToTVBModels
+
+    _input_proxy_models = NetpyneInputProxyModels
+    _output_proxy_models = NetpyneOutputProxyModels
+
+    _output_interface_type = NetpyneOutputInterface
+    _input_interface_type = NetpyneInputInterface
+
+    config = Attr(
+        label="Configuration",
+        field_type=Config,
+        doc="""Configuration class instance.""",
+        required=True,
+        default=CONFIGURED
+    )
+
+    logger = Attr(
+        label="Logger",
+        field_type=Logger,
+        doc="""logging.Logger instance.""",
+        required=True,
+        default=initialize_logger(__name__, config=CONFIGURED)
+    )
+
+    def _get_tvb_delays(self):
+        return np.maximum(self.spiking_dt,
+                          SpikeNetInterfaceBuilder._get_tvb_delays(self) - self.spiking_dt).astype("float32")
+
+
+class NetpyneRemoteInterfaceBuilder(NetpyneInterfaceBuilder, SpikeNetRemoteInterfaceBuilder):
+
+    """NetpyneRemoteInterfaceBuilder class"""
+
+    # _output_interface_type = NESTSenderInterface
+    # _input_interface_type = NESTReceiverInterface
+
+    def configure(self):
+        SpikeNetRemoteInterfaceBuilder.configure(self)
+
+
+class TVBNetpyneInterfaceBuilder(NetpyneProxyNodesBuilder, TVBSpikeNetInterfaceBuilder):
+
+    """TVBNetpyneInterfaceBuilder class"""
+
+    _tvb_to_spikeNet_models = TVBtoNetpyneModels
+    _spikeNet_to_TVB_models = NetpyneToTVBModels
+
+    _default_spikeNet_to_tvb_models = DefaultNetpyneToTVBModels
+    _default_tvb_to_spikeNet_models = DefaultTVBtoNetpyneModels
+
+    _input_proxy_models = NetpyneOutputProxyModels  # Input to SpikeNet is output of TVB
+    _output_proxy_models = NetpyneInputProxyModels  # Output of SpikeNet is input to TVB
+
+    _output_interface_type = TVBtoNetpyneInterface
+    _input_interface_type = NetpyneToTVBInterface
+
+    def configure(self):
+        TVBSpikeNetInterfaceBuilder.configure(self)
+        
+        adjusted_scale = .0
+        for interface in self.output_interfaces:
+            adjusted_scale += interface["transformer"].scale_factor
+        adjusted_scale *= len(self.output_interfaces)
+
+        for interface in self.output_interfaces:
+            interface["transformer"].adjusted_scale_factor = adjusted_scale
+
+
+    def _get_tvb_delays(self):
+        return (np.maximum(1,
+                           np.rint((TVBSpikeNetInterfaceBuilder._get_tvb_delays(self)
+                                    - self.synchronization_time + self.spiking_dt)/self.spiking_dt).astype("i")
+                           ) * self.spiking_dt).astype("float32")
---
+++ tvb_multiscale/tvb_netpyne/netpyne_models/network.py
@@ -0,0 +1,48 @@
+from tvb_multiscale.core.spiking_models.network import SpikingNetwork
+from tvb_multiscale.tvb_netpyne.netpyne.instance import NetpyneInstance
+
+from tvb_multiscale.tvb_netpyne.config import CONFIGURED, initialize_logger
+from tvb_multiscale.tvb_netpyne.netpyne_models.devices import NetpyneOutputSpikeDeviceDict
+
+class NetpyneNetwork(SpikingNetwork):
+
+    """
+        NetpyneNetwork is a class representing a spiking network comprising of:
+        - a SpikingBrain class, i.e., neural populations organized per brain region they reside and neural model,
+        - a pandas.Series of DeviceSet classes of output (measuring/recording/monitor) devices,
+        - a pandas.Series of DeviceSet classes of input (stimulating) devices,
+        all of which are implemented as indexed mappings by inheriting from pandas.Series class.
+        The class also includes methods to return measurements (mean, sum/total data, spikes, spikes rates etc)
+        from output devices, as xarray.DataArrays.
+        e.g. SpikingPopulations can be indexed as:
+        spiking_network.brain_regions['rh-insula']['E'] for population "E" residing in region node "rh-insula",
+        and similarly for an output device:
+        spiking_network.output_devices['Excitatory']['rh-insula'], 
+        which measures a quantity labelled following the target population ("Excitatory"),
+        residing in region node "rh-insula".
+    """
+
+    netpyne_instance = None
+
+    _OutputSpikeDeviceDict = NetpyneOutputSpikeDeviceDict
+
+    def __init__(self, netpyne_instance, **kwargs):
+        self.netpyne_instance = netpyne_instance
+        super(NetpyneNetwork, self).__init__(**kwargs)
+
+    @property
+    def spiking_simulator_module(self):
+        return self.netpyne_instance
+
+    @property
+    def dt(self):
+        return self.netpyne_instance.dt
+
+    @property
+    def min_delay(self):
+        return self.netpyne_instance.minDelay()
+
+    def Run(self, simulation_length, *args, **kwargs):
+        """Method to simulate the NetPyNE network for a specific simulation_length (in ms).
+        """
+        self.netpyne_instance.run(simulation_length)
