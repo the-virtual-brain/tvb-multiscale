af745583ff5bdbb06fec51b3206e0511d77f1397
dionperd <dionperd@gmail.com>
2022-06-24 16:32:29 +0200

    Ongoing work on cluster BIH script

--- examples/tvb_nest/notebooks/cerebellum/run_script_bih_singularity.sh
+++ examples/tvb_nest/notebooks/cerebellum/run_script_bih_singularity.sh
@@ -4,24 +4,20 @@
 #SBATCH --mail-user=dionperd@gmail.com
 #SBATCH --time=00:30:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-core=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --partition=normal
-#SBATCH --constraint=mc
-#SBATCH --hint=nomultithread
+#SBATCH --ntasks=1
+#SBATCH --partition=medium
 #SBATCH --mem-per-cpu=1G
 
 
-# export SCRATCH=/scratch/snx3000/bp000229
-#export TVB_ROOT=$SCRATCH/Software/TVB/tvb-root
-#export TVB_MULTISCALE=$SCRATCH/Software/TVB/tvb-multiscale
+export WORK=/data/gpfs-1/users/perdikid_c/work
+#export TVB_ROOT=$WORK/tvb-root
+# export TVB_MULTISCALE=$$WORK/tvb-multiscale
 export HOME_DOCKER=/home/docker
 export PYTHON=$HOME_DOCKER/env/neurosci/bin/python
 export DOCKER_ROOT=$HOME_DOCKER/packages/tvb-root
 export DOCKER_MULTISCALE=$HOME_DOCKER/packages/tvb-multiscale
 export WORKDIR=$DOCKER_MULTISCALE/examples/tvb_nest/notebooks/cerebellum
-export IMAGE=tvb_multiscale_dev_parallel_cluster
+export IMAGE=$WORK/tvb_multiscale_dev_parallel_cluster
 export SBIFIT=$WORKDIR/scripts.py
 export G=1
 
