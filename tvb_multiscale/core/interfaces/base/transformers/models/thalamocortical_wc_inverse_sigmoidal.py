dfd5b21f3b5dbdac98c6f2eae9be2774763ac6f6
dionperd <dionperd@gmail.com>
2022-07-15 15:56:27 +0300

    Added thalamocortical_wc_inverse_sigmoidal.py transformer

---
+++ tvb_multiscale/core/interfaces/base/transformers/models/thalamocortical_wc_inverse_sigmoidal.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, ABC
+from enum import Enum
+
+import numpy as np
+
+from tvb.basic.neotraits.api import HasTraits
+from tvb.basic.neotraits._attr import NArray
+
+from tvb_multiscale.core.interfaces.base.transformers.models.base import SpikesToRates
+from tvb_multiscale.core.interfaces.base.transformers.models.elephant import \
+    ElephantSpikesHistogram, ElephantSpikesHistogramRate, ElephantSpikesRate
+
+
+class ThalamoCorticalWCInverseSigmoidal(HasTraits):
+
+    Rmin = NArray(
+        label=r":math:`R_max`",
+        default=np.array([0.1, ]),
+        doc="""[Hz]. Minimum rate.""")
+
+    beta = NArray(
+        label=r":math:`\beta`",
+        default=np.array([0.1, ]),
+        doc="""[Hz]. Maximum rate.""")
+
+    sigma = NArray(
+        label=r":math:sigma`",
+        default=np.array([0.0, ]),
+        doc="""[mV] Firing threshold (PSP) for which a 50% firing rate is achieved.
+                In other words, it is the value of the average membrane potential
+                corresponding to the inflection point of the sigmoid.
+                The usual value for this parameter is around 6.0.""")
+
+    beta = NArray(
+        label=r":math:`\beta`",
+        default=np.array([20.0, ]),
+        doc="""Steepness of the sigmoidal transformation.""")
+
+    def _compute(self, input_buffer):
+        return self.sigma - np.log(1.0 / np.maximum(input_buffer, self.Rmin) - 1) / self.beta
+
+
+class SpikesToRatesThalamoCorticalWCInverseSigmoidal(SpikesToRates, ThalamoCorticalWCInverseSigmoidal, ABC):
+    __metaclass__ = ABCMeta
+
+    def configure(self):
+        super().configure()
+        ThalamoCorticalWCInverseSigmoidal.configure(self)
+
+    def _compute(self, input_buffer, *args, **kwargs):
+        """Method for the computation on the input buffer spikes' trains' data
+           for the output buffer data of instantaneous mean spiking rates to result."""
+        return ThalamoCorticalWCInverseSigmoidal._compute(self, super()._compute(input_buffer, *args, **kwargs))
+
+
+class ElephantSpikesHistogramThalamoCorticalWCInverseSigmoidal(ElephantSpikesHistogram,
+                                                       SpikesToRatesThalamoCorticalWCInverseSigmoidal,
+                                                       ThalamoCorticalWCInverseSigmoidal):
+
+    def configure(self):
+        SpikesToRatesThalamoCorticalWCInverseSigmoidal.configure(self)
+
+    def _compute(self, input_buffer, *args, **kwargs):
+        """Method for the computation on the input buffer spikes' trains' data
+           for the output buffer data of instantaneous mean spiking rates to result."""
+        return SpikesToRatesThalamoCorticalWCInverseSigmoidal._compute(self, input_buffer, *args, **kwargs)
+
+
+class ElephantSpikesHistogramRateThalamoCorticalWCInverseSigmoidal(ElephantSpikesHistogramRate,
+                                                           SpikesToRatesThalamoCorticalWCInverseSigmoidal,
+                                                           ThalamoCorticalWCInverseSigmoidal):
+
+    def configure(self):
+        SpikesToRatesThalamoCorticalWCInverseSigmoidal.configure(self)
+
+    def _compute(self, input_buffer, *args, **kwargs):
+        """Method for the computation on the input buffer spikes' trains' data
+           for the output buffer data of instantaneous mean spiking rates to result."""
+        return SpikesToRatesThalamoCorticalWCInverseSigmoidal._compute(self, input_buffer, *args, **kwargs)
+
+
+class ElephantSpikesRateThalamoCorticalWCInverseSigmoidal(ElephantSpikesRate,
+                                                          SpikesToRatesThalamoCorticalWCInverseSigmoidal,
+                                                          ThalamoCorticalWCInverseSigmoidal):
+
+    def configure(self):
+        SpikesToRatesThalamoCorticalWCInverseSigmoidal.configure(self)
+
+    def _compute(self, input_buffer, *args, **kwargs):
+        """Method for the computation on the input buffer spikes' trains' data
+           for the output buffer data of instantaneous mean spiking rates to result."""
+        return SpikesToRatesThalamoCorticalWCInverseSigmoidal._compute(self, input_buffer, *args, **kwargs)
