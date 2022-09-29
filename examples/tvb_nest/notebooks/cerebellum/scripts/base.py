982ca0777b1916c8bbafa179fd0ba05ac6a66833
dionperd <dionperd@gmail.com>
2022-09-29 13:54:13 +0200

    Prepared 1D STIM, Is fitting

--- examples/tvb_nest/notebooks/cerebellum/scripts/base.py
+++ examples/tvb_nest/notebooks/cerebellum/scripts/base.py
@@ -20,11 +20,11 @@ TvbProfile.set_profile(TvbProfile.LIBRARY_PROFILE)
 from tvb.simulator.integrators import EulerStochastic
 
 
-DEFAULT_ARGS = {'G': 2.0, 'STIMULUS': 0.5,
-                'I_e': -0.25, 'I_s': 0.25,
+DEFAULT_ARGS = {'G': 0.0, 'STIMULUS': 0.5,
+                'I_e': -0.35, 'I_s': 0.5,
                 'w_ie': -3.0, 'w_rs': -2.0,
                 'CONN_LOG': True, 'FIC': 0, 'PRIORS_DIST': 'uniform',
-                'output_folder': '', 'verbose': 1, 'return_plotter': True}
+                'output_folder': 'cwc1D_STIM_Is', 'verbose': 1, 'return_plotter': True}
 
 
 def create_plotter(config):
@@ -157,15 +157,15 @@ def configure(**ARGS):
     config.TARGET_FREQS = np.arange(5.0, 48.0, 1.0)
     config.POSTERIOR_SAMPLES_PATH = os.path.join(config.out.FOLDER_RES, "samples_fit.npy")
     config.N_FIT_RUNS = 10  # 3 - 10
-    config.N_SIMULATIONS = 1000
-    config.N_SIM_BATCHES = 10
+    config.N_SIMULATIONS = 900
+    config.N_SIM_BATCHES = 30
     config.SPLIT_RUN_SAMPLES = 2
     config.N_SAMPLES_PER_RUN = 1000
     config.BATCH_FILE_FORMAT = "%s_%03d%s"
     config.BATCH_FILE_FORMAT_G = "%s_iG%02d_%03d%s"
     config.BATCH_PRIORS_SAMPLES_FILE = "bps.pt"  # bps_iG01_iB010.pt
     config.BATCH_SIM_RES_FILE = "bsr.npy"  # bsr_iG01_iB010.npy
-    config.Gs = np.array([0.1, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 7.5, 10.0])
+    config.Gs = np.array([0.0])  # np.array([0.1, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 7.5, 10.0])
     config.PRIORS_DIST = args['PRIORS_DIST']  # "normal" or "uniform"
     config.PRIORS_DEF = \
         {"STIMULUS": {"min": 0.0, "max": 1.0, "loc": 0.5, "sc": 0.1},
