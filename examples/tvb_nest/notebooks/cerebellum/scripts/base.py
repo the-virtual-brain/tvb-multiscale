3f7df726927a49b9814582bc864cbe542b38e3f3
dionperd <dionperd@gmail.com>
2022-09-30 18:56:22 +0200

    Added posterior filepath to configuration

--- examples/tvb_nest/notebooks/cerebellum/scripts/base.py
+++ examples/tvb_nest/notebooks/cerebellum/scripts/base.py
@@ -155,6 +155,7 @@ def configure(**ARGS):
     config.TARGET_PSD_POPA_PATH = popa_freqs_path
     config.PSD_TARGET_PATH = os.path.join(config.out.FOLDER_RES, "PSD_target.npy")
     config.TARGET_FREQS = np.arange(5.0, 48.0, 1.0)
+    config.POSTERIOR_PATH = os.path.join(config.out.FOLDER_RES, "posterior.pkl")
     config.POSTERIOR_SAMPLES_PATH = os.path.join(config.out.FOLDER_RES, "samples_fit.npy")
     config.N_FIT_RUNS = 10  # 3 - 10
     config.N_SIMULATIONS = 900
