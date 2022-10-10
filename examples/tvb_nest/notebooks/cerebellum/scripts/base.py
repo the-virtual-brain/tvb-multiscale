3746c9ab445ce531f3f51d7ef487a113df5e68fb
dionperd <dionperd@gmail.com>
2022-10-10 21:53:26 +0200

    Removed cwc_STIM_is_Wie.ipynb

--- examples/tvb_nest/notebooks/cerebellum/scripts/base.py
+++ examples/tvb_nest/notebooks/cerebellum/scripts/base.py
@@ -23,7 +23,7 @@ from tvb.simulator.integrators import EulerStochastic
 DEFAULT_ARGS = {'G': 0.0, 'STIMULUS': 0.5,
                 'I_e': -0.35, 'I_s': 0.5,
                 'w_ie': -3.0, 'w_rs': -2.0,
-                'CONN_LOG': True, 'FIC': 'fit', 'PRIORS_DIST': 'uniform',
+                'CONN_LOG': True, 'FIC': 0, 'PRIORS_DIST': 'uniform',
                 'output_folder': 'cwc_STIM_Is_Wie', 'verbose': 1, 'plot_flag': True}
 
 
