3d2aaabfffc0a865e73c4baf92f03b9731f7811c
dionperd <dionperd@gmail.com>
2022-10-13 10:46:59 +0200

    Ongoing fit cwc_STIM

--- examples/tvb_nest/notebooks/cerebellum/scripts/base.py
+++ examples/tvb_nest/notebooks/cerebellum/scripts/base.py
@@ -21,10 +21,10 @@ from tvb.simulator.integrators import EulerStochastic
 
 
 DEFAULT_ARGS = {'G': 1.0, 'STIMULUS': 0.5,
-                'I_e': -0.35, 'I_s': 0.2,
+                'I_e': -0.35, 'I_s': 0.08,
                 'w_ie': -3.0, 'w_rs': -2.0,
                 'CONN_LOG': True, 'FIC': 0, 'PRIORS_DIST': 'uniform',
-                'output_folder': 'cwc1D_Is', 'verbose': 1, 'plot_flag': True}
+                'output_folder': 'cwc_STIM', 'verbose': 1, 'plot_flag': True}
 
 
 def create_plotter(config):
@@ -156,15 +156,15 @@ def configure(**ARGS):
     config.POSTERIOR_PATH = os.path.join(config.out.FOLDER_RES, "posterior.pkl")
     config.POSTERIOR_SAMPLES_PATH = os.path.join(config.out.FOLDER_RES, "samples_fit.npy")
     config.N_FIT_RUNS = 10  # 3 - 10
-    config.N_SIMULATIONS = 900
-    config.N_SIM_BATCHES = 30
+    config.N_SIMULATIONS = 1000
+    config.N_SIM_BATCHES = 10
     config.SPLIT_RUN_SAMPLES = 2
     config.N_SAMPLES_PER_RUN = 1000
     config.BATCH_FILE_FORMAT = "%s_%03d%s"
     config.BATCH_FILE_FORMAT_G = "%s_iG%02d_%03d%s"
     config.BATCH_PRIORS_SAMPLES_FILE = "bps.pt"  # bps_iG01_iB010.pt
     config.BATCH_SIM_RES_FILE = "bsr.npy"  # bsr_iG01_iB010.npy
-    config.Gs = np.array([0.0]) # np.array([0.1, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 7.5, 10.0]) 
+    config.Gs = np.array([0.1, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]) 
     config.PRIORS_DIST = args['PRIORS_DIST']  # "normal" or "uniform"
     config.PRIORS_DEF = \
         {"STIMULUS": {"min": 0.0, "max": 1.0, "loc": 0.5, "sc": 0.1},
@@ -174,7 +174,7 @@ def configure(**ARGS):
          "w_rs": {"min": -4.0, "max": 0.0, "loc": -2.0, "sc": 0.5},
          "FIC": {"min": 0.0, "max": 25.0, "loc": 10.0, "sc": 5.0},
         }
-    config.PRIORS_PARAMS_NAMES = ['I_s']  #'STIMULUS', 'I_e', 'w_ie', 'w_rs', 'FIC',
+    config.PRIORS_PARAMS_NAMES = ['STIMULUS']  # , 'I_e', 'I_s', 'w_ie', 'w_rs', 'FIC',
     if config.FIC == "fit":
         config.FIC = 1.0
         config.PRIORS_PARAMS_NAMES.append("FIC")
