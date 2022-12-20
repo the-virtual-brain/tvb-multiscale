7b7de092732c2bf9c7a6897212f669fda5226c32
jchen6727 <jchen.6727@gmail.com>
2022-12-20 16:27:53 -0500

    builder testing
    test_spiking_models_builder_properties_methods.py
    added test case for netpyne builder.
    not documented yet

--- tests/tvb_netpyne/test_spiking_models_builder_properties_methods.py
+++ tests/tvb_netpyne/test_spiking_models_builder_properties_methods.py
@@ -1,10 +1,34 @@
-import unittest
+from tvb.basic.profile import TvbProfile
+TvbProfile.set_profile(TvbProfile.LIBRARY_PROFILE)
 
+from tvb_multiscale.tvb_netpyne.config import CONFIGURED, Config
+from tvb_multiscale.tvb_netpyne.netpyne_models.builders.netpyne_factory import load_netpyne
+from tvb_multiscale.tvb_netpyne.netpyne_models.models.default_exc_io_inh_i import DefaultExcIOInhIBuilder
+from tvb_multiscale.tvb_netpyne.netpyne_models.models.wilson_cowan import WilsonCowanBuilder
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from tvb.datatypes.connectivity import Connectivity
+from tvb.simulator.simulator import Simulator
+from tvb_multiscale.core.tvb.cosimulator.models.linear import Linear
+from tvb.simulator.monitors import Raw
 
+def test( dt = 0.1, duration = 100, config=CONFIGURED ):
+    connectivity = Connectivity.from_file(CONFIGURED.DEFAULT_CONNECTIVITY_ZIP)
+    connectivity.configure()
 
-if __name__ == '__main__':
-    unittest.main()
+    nodes = [ 0 ] # tvb-multiscale/examples/data/tvb_data/berlinSubjects/QL_20120814 ?
+
+    simulator = Simulator()
+    simulator.integrator.dt = dt
+    simulator.model = Linear()
+
+    simulator.connectivity = connectivity
+    mon_raw = Raw(period = simulator.integrator.dt )
+    simulator.monitors = (mon_raw,)
+
+    config.simulation_length = duration
+    netpyne_model_builder = DefaultExcIOInhIBuilder(simulator, nodes, netpyne_instance=(load_netpyne()), config=config)
+    netpyne_model_builder.configure()
+    print(netpyne_model_builder.info())
+
+if __name__ == "__main__":
+    test()
