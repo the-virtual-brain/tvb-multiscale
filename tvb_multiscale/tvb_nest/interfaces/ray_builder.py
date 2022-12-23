1c26e269d3a63e926456213740c7a32e4972b08a
dionperd <dionperd@gmail.com>
2022-12-23 18:07:30 +0200

    Added spiking_network as RayNESTNetwork client in tvb_nest.interfaces.ray_builder

--- tvb_multiscale/tvb_nest/interfaces/ray_builder.py
+++ tvb_multiscale/tvb_nest/interfaces/ray_builder.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from tvb.basic.neotraits.api import Attr
+
 from tvb_multiscale.core.interfaces.tvb.ray_builder import \
     RayTVBSpikeNetInterfaceBuilder, create_ray_TVB_spikeNet_interface_builder_type
+from tvb_multiscale.tvb_nest.nest_models.ray import RayNESTNetwork
 from tvb_multiscale.tvb_nest.interfaces.ray_interfaces import \
     RayTVBtoNESTInterface, RayNESTtoTVBInterface, RayTVBtoNESTInterfaces, RayNESTtoTVBInterfaces
 from tvb_multiscale.tvb_nest.interfaces.builders import TVBNESTInterfaceBuilder
@@ -14,6 +17,11 @@ class RayTVBNESTInterfaceBuilder(TVBNESTInterfaceBuilder, RayTVBSpikeNetInterfac
 
     _output_interface_type = RayTVBtoNESTInterface
     _input_interface_type = RayNESTtoTVBInterface
+
+    spiking_network = Attr(label="NEST Network Ray client",
+                           doc="""A Ray client to an instance of NESTNetwork class""",
+                           field_type=RayNESTNetwork,
+                           required=True)
 
     def _configure_transformer_model(self, interface, interface_models, default_transformer_models, transformer_models):
         RayTVBSpikeNetInterfaceBuilder._configure_transformer_model(
