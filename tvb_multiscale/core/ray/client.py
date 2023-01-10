738ca53bfa38f50e0ea62c12eb9788a75ac88f66
dionperd <dionperd@gmail.com>
2023-01-10 19:00:14 +0100

    Revert "Loggging and raising error in case of some NaN values in cosim_updates of cosimulator.py"
    This reverts commit fffa29882faba892fb1fea4a90d2b61d7569d1b5.

--- tvb_multiscale/core/ray/client.py
+++ tvb_multiscale/core/ray/client.py
@@ -9,9 +9,7 @@ from tvb_multiscale.core.ray.server import create_ray_server
 
 class RayClient(object):
 
-    ray_server = None
-
-    def __init__(self, ray_server=None):
+    def __init__(self, ray_server):
         self.ray_server = ray_server
         super(RayClient, self).__init__()
 
@@ -19,7 +17,7 @@ class RayClient(object):
         return ray.get(self.ray_server.__getattribute__.remote(attr))
 
     def __setattr__(self, attr, value):
-        if attr == "ray_server":
+        if attr.find("ray_server") > -1:
             super(RayClient, self).__setattr__(attr, value)
         else:
             ray.get(self.ray_server.__setattr__.remote(attr, value))
@@ -33,11 +31,11 @@ class RayClient(object):
 
 def create_ray_client_function(name, parallel=False):
 
-    def ray_function(self, *args, **kwargs):
-        return ray.get(getattr(self.ray_server, name).remote(*args, **kwargs))
+    def ray_function(cls, *args, **kwargs):
+        return ray.get(getattr(cls.ray_server, name).remote(*args, **kwargs))
 
-    def ray_parallel_function(self, *args, **kwargs):
-        return getattr(self.ray_server, name).remote(*args, **kwargs)
+    def ray_parallel_function(cls, *args, **kwargs):
+        return getattr(cls.ray_server, name).remote(*args, **kwargs)
 
     if parallel:
         return ray_parallel_function
@@ -45,29 +43,24 @@ def create_ray_client_function(name, parallel=False):
         return ray_function
 
 
-def add_server_methods_to_client(ray_client, ray_server, input_class, non_blocking_methods=[]):
-
-    for server_method in ray_server.__dict__['_ray_method_signatures']:
-        if hasattr(input_class, server_method) and \
-                server_method not in ["__init__", "__getattribute__", "__getattr__", "__setattr__"]:
-            if server_method in non_blocking_methods:
-                fun = lambda self, *args, **kwargs: \
-                    getattr(self.ray_server, server_method).remote(*args, **kwargs)
-            else:
-                fun = lambda self, *args, **kwargs: \
-                    ray.get(getattr(self.ray_server, server_method).remote(*args, **kwargs))
-            if isinstance(getattr(input_class, server_method), property):
-                setattr(ray_client, server_method, property(fun))
-            else:
-                setattr(ray_client, server_method, MethodType(fun, ray_client))
-
-    return ray_client
-
-
 def create_ray_client(input_class, client_type=RayClient, non_blocking_methods=[], *args, **kwargs):
 
     ray_server = create_ray_server(input_class, *args, **kwargs)
 
     # RayClient.__name___ = "Ray%s" % input_class.__name__
+    client_type.ray_server = ray_server
 
-    return add_server_methods_to_client(client_type(ray_server), ray_server, input_class, non_blocking_methods)
+    for server_method in ray_server.__dict__['_ray_method_signatures']:
+        if hasattr(input_class, server_method) and \
+                server_method not in ["__init__", "__getattribute__", "__getattr__", "__setattr__"]:
+            if server_method in non_blocking_methods:
+                fun = create_ray_client_function(server_method, True)
+            else:
+                fun = create_ray_client_function(server_method, False)
+            if isinstance(getattr(input_class, server_method), property):
+                setattr(client_type, server_method, property(fun))
+            else:
+                setattr(client_type, server_method, MethodType(fun, client_type))
+
+    return client_type(ray_server)
+
