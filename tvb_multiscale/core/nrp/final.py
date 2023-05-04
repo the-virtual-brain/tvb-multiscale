2298fd103c9d9e9e7dccaf711cb9a64f7c7b453c
dionperd <dionperd@gmail.com>
2023-05-04 22:27:03 +0300

    Added nrp.final

---
+++ tvb_multiscale/core/nrp/final.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def tvb(tvb_app):
+    return tvb_app.stop()
+
+
+def spikeNet(spikeNet_app):
+    return spikeNet_app.stop()
+
+
+def transformer(transformer_app):
+    return transformer_app.stop()
