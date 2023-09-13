19a6cc85e46c581d3d30dd629a564307478deb7a
dionperd <dionperd@gmail.com>
2023-09-13 19:03:01 +0200

    Commented initial condition setting from simulator configuration of tvb_app

--- tvb_multiscale/core/orchestrators/tvb_app.py
+++ tvb_multiscale/core/orchestrators/tvb_app.py
@@ -323,8 +323,8 @@ class TVBParallelApp(TVBApp):
         self._xs = xs
         if not self.cosimulator.n_tvb_steps_ran_since_last_synch:
             self.cosimulator.n_tvb_steps_ran_since_last_synch = int(self.cosimulator.synchronization_n_step)
-        # Send TVB's initial condition to spikeNet!:
-        return self.get_tvb_init_cosim_coupling()
+        # # Send TVB's initial condition to spikeNet!:
+        # return self.get_tvb_init_cosim_coupling()
 
     def run_for_synchronization_time(self, cosim_updates=None, cosimulation=True):
         return self.cosimulator.run_for_synchronization_time(
