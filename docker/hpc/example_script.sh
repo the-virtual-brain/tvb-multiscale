150f24c90120913c1f65dfbbcfa6ae32a86da837
dionperd <dionperd@gmail.com>
2023-09-21 18:15:45 +0200

    Updates to docker/hpc/example_script.sh

--- docker/hpc/example_script.sh
+++ docker/hpc/example_script.sh
@@ -10,7 +10,7 @@ export DOCKER_HOME=/home/docker
 export DOCKER_TVB_ROOT=$DOCKER_HOME/packages/tvb-root
 export DOCKER_TVB_MULTISCALE=$DOCKER_HOME/packages/tvb-multiscale
 export DOCKER_WORKDIR=$DOCKER_TVB_MULTISCALE/tvb_multiscale/examples/tvb_nest
-export DOCKER_PYTHON=/home/docker/env/neurosci/bin/python
+export DOCKER_PYTHON=/usr/bin/python
 export DOCKER_RUN_FILE=/home/docker/packages/tvb-multiscale/tvb_multiscale/examples/tvb_nest/example.py
 export CMD=$DOCKER_PYTHON' '$DOCKER_RUN_FILE
 
@@ -25,7 +25,7 @@ export SRUN='srun -C '$NODES' -t '$TIME' -o '$OUTPUT_FILE' --mail-type=ALL --mai
 ## In PizDaint, we assume that these lines have been already ran:
 #module load daint-mc # or daint-gpu
 #module load sarus
-export DOCKER_IMAGE='thevirtualbrain/tvb-nest:ongoing_dp_work'
+export DOCKER_IMAGE='thevirtualbrain/tvb-nest:dev'
 
 export INTERACTIVE_CMD='sarus --debug run -t --mpi --workdir='$DOCKER_WORKDIR'
                               --mount=type=bind,source=${TVB_MULTISCALE},destination=$'DOCKER_TVB_MULTISCALE'
@@ -46,7 +46,7 @@ export SRUN_COMMAND=$SRUN' '$COMMAND
 ##module load daint-mc # or daint-gpu
 ##module load singularity/3.5.3-daint
 ## singularity pull docker://${DOCKER_IMAGE}
-#export DOCKER_IMAGE_RUN='tvb-nest_dp-ongoing-work.sif'
+#export DOCKER_IMAGE_RUN='tvb-nest_dev.sif'
 #
 ## Run from within the path where the .sif file is:
 #
