6104af0af59e65e662e7ea7e6deb8fe53af87033
dionperd <dionperd@gmail.com>
2023-09-25 19:38:41 +0200

    Updated rayfy to work also for HasTraits instances

--- tvb_multiscale/core/ray/server.py
+++ tvb_multiscale/core/ray/server.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import ray
+from tvb_multiscale.core.neotraits import HasTraits
 
 
 class RayServer(object):
@@ -12,18 +13,29 @@ class RayServer(object):
         return super(RayServer, self).__setattr__(attr, val)
 
     def __getstate__(self):
-        return {}
+        d ={}
+        for key, val in self.__dict__.items():
+            if key[0] != "_" and not callable(getattr(self, key)):
+                d[key] = val
+        return d
 
     def __setstate__(self, d):
-        pass
+        for key, val in d.items():
+            setattr(self, key, val)
 
 
 def rayfy(input_class, ray_server_base=RayServer):
-    d = dict(ray_server_base.__dict__)
-    d.update(dict(input_class.__dict__))
-    new_ray_server_type_name = "%s%s" % (ray_server_base.__name__, input_class.__name__)
-    new_ray_server_type = type(new_ray_server_type_name, (input_class, ray_server_base,), d)
-    return ray.remote(new_ray_server_type)
+    if not isinstance(input_class, HasTraits) and not issubclass(input_class, HasTraits):
+        d = dict(ray_server_base.__dict__)
+        d.update(dict(input_class.__dict__))
+        new_ray_server_type_name = "%s%s" % (ray_server_base.__name__, input_class.__name__)
+        remote_class = type(new_ray_server_type_name, (input_class, ray_server_base,), d)
+    else:
+        remote_class = input_class
+    for iP, parent in enumerate(remote_class.__mro__):
+        if iP > 0:
+            ray.remote(parent)
+    return ray.remote(remote_class)
 
 
 def create_ray_server(input_class, *args, **kwargs):
