4d841bc2c7c64204f62da0dbb24035f56dcc21fb
dionperd <dionperd@gmail.com>
2023-09-28 15:14:38 +0200

    Added __init__ for ANNarchy Network models and base builder

--- tvb_multiscale/tvb_annarchy/annarchy_models/network.py
+++ tvb_multiscale/tvb_annarchy/annarchy_models/network.py
@@ -5,13 +5,10 @@ import os
 from tvb.basic.neotraits.api import Attr
 
 from tvb_multiscale.core.spiking_models.network import SpikingNetwork
-from tvb_multiscale.tvb_annarchy.config import Config, CONFIGURED, initialize_logger
+from tvb_multiscale.tvb_annarchy.config import Config, CONFIGURED
 from tvb_multiscale.tvb_annarchy.annarchy_models.brain import ANNarchyBrain
 from tvb_multiscale.tvb_annarchy.annarchy_models.devices import \
     ANNarchyOutputSpikeDeviceDict, ANNarchyOutputContinuousTimeDeviceDict
-
-
-LOG = initialize_logger(__name__)
 
 
 class ANNarchyNetwork(SpikingNetwork):
@@ -67,6 +64,10 @@ class ANNarchyNetwork(SpikingNetwork):
 
     def __init__(self, annarchy_instance=None, **kwargs):
         self.annarchy_instance = annarchy_instance
+        self._dt = None
+        self._network = None
+        self.network_path = ""
+        self.brain_regions = ANNarchyBrain()
         super(ANNarchyNetwork, self).__init__(**kwargs)
 
     @property
