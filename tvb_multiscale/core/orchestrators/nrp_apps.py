d5553d2cabc166626dd64391b157743035d0152d
dionperd <dionperd@gmail.com>
2023-11-02 20:33:25 +0100

    Corrected nrp_apps.py for DefaultTVBInterfaceBuilder

--- tvb_multiscale/core/orchestrators/nrp_apps.py
+++ tvb_multiscale/core/orchestrators/nrp_apps.py
@@ -6,20 +6,18 @@ from tvb_multiscale.core.neotraits import HasTraits
 from tvb_multiscale.core.tvb.cosimulator.cosimulator_parallel import CoSimulatorParallel
 from tvb_multiscale.core.tvb.cosimulator.cosimulator_builder import CoSimulatorParallelBuilder
 from tvb_multiscale.core.interfaces.models.default import \
-    DefaultTVBSpikeNetInterfaceBuilder, DefaultTVBRemoteInterfaceBuilder
+    DefaultTVBSpikeNetInterfaceBuilder, DefaultTVBInterfaceBuilder
 from tvb_multiscale.core.orchestrators.tvb_app import TVBParallelApp
 from tvb_multiscale.core.orchestrators.spikeNet_app import SpikeNetParallelApp
 from tvb_multiscale.core.orchestrators.transformer_app import \
     TransformerApp, TVBtoSpikeNetTransformerApp, SpikeNetToTVBTransformerApp
 
 
-
-
 class NRPTVBApp(TVBParallelApp):
 
     """NRPTVBApp class"""
 
-    _default_interface_builder = DefaultTVBRemoteInterfaceBuilder
+    _default_interface_builder = DefaultTVBInterfaceBuilder
 
 
 class NRPSpikeNetApp(SpikeNetParallelApp):
